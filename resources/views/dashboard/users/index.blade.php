772333f6a4b316d6e5b72003ce0d1f710134cf8c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 02:02:54 +0600

    Commit Wed 10/22/2025  2:02:54.28 23184

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -547,7 +547,9 @@
 @endsection
 
 @section('third_party_scripts')
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript">
         $('#adduserrole').change(function () {
             if($('#adduserrole').val() == 'accountant') {
