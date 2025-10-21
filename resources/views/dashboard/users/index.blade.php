6e71207f26337938a2d6de99b4bdad02f6f2c5f9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 03:22:35 +0600

    Commit Wed 10/22/2025  3:22:35.30 28194

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -639,7 +639,7 @@
           // --- 2. Change Event Listener (Cascading Dropdowns) ---
           // Applies to both Add and Edit modals
           $(document).on('change', '.authority-select', function() {
-              const userId = $(this).val();
+              const userId = $(this).data('target');
               const parentId = $(this).val();
               const targetId = $(this).data('target');
               const modelName = $(this).data('model');
