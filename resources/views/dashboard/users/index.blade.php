a7a7299347e2499de83afc98d863bf6c1e12eaa4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 01:57:43 +0600

    Commit Wed 10/22/2025  1:57:43.27 27465

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -351,7 +351,7 @@
 	                		<option selected="" disabled="" value="">ধরন</option>
 	                		<option value="admin">এডমিন</option>
 							       <option value="manager">স্থানীয় সরকার প্রতিনিধি</option>
-                     <option value="volunteer">ভলান্টিয়ার</option>
+                     {{-- <option value="volunteer">ভলান্টিয়ার</option> --}}
 	                		<option value="user">ব্যবহারকারী</option>
 							{{-- <option value="accountant">একাউন্টেন্ট</option> --}}
 	                	</select>
