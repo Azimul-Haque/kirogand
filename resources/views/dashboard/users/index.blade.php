d2f70e5d3a5dc62f0c0dbce2f14b713ec6e10b99
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 03:13:42 +0600

    Commit Wed 10/22/2025  3:13:42.30 8419

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -650,6 +650,7 @@
               if (parentId) {
                   $('#' + context + '_authority_level').val(level);
                   $('#' + context + '_authority_id').val(parentId);
+                  console.log(context).val());
                   console.log($('#' + context + '_authority_level').val());
               } else {
                   // If the selected value is empty, reset the current authority level/id
