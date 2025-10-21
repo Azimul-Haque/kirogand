cdb85b3e72c524e6b8de52fabf8c015a31f49395
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 21:55:49 +0600

    Commit Tue 10/21/2025 21:55:49.26 11133

--- app/Division.php
+++ app/Division.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Division extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'bn_name',
+        'url',
+    ];
+
+    /**
+     * Get the districts for the division.
+     */
+    public function districts(): HasMany
+    {
+        return $this->hasMany(District::class);
+    }
 }
