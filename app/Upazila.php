1955ec3e998b44f583e4de096bd8057d35420ffe
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 22:49:39 +0600

    Commit Tue 10/21/2025 22:49:39.26 4398

--- app/Upazila.php
+++ app/Upazila.php
@@ -1,11 +1,7 @@
 <?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+namespace App;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Upazila extends Model
 {
