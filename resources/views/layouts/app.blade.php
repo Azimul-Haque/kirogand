4156cda25ff515b47867687c920d6062771b3d51
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 04:16:56 +0600

    Commit Wed 10/22/2025  4:16:56.24 12300

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,9 @@
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
         </ul>
-        আসদাস
+        @if(Auth::user()->role == 'manager')
+
+        @endif
 
         <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
