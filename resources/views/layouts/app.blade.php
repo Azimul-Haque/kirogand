bb7a5f0fa288b71a45547f0c2245856d25170fbf
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 04:16:08 +0600

    Commit Wed 10/22/2025  4:16:08.23 3549

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
         </ul>
+        আসদাস
 
         <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
