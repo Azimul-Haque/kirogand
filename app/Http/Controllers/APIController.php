b7020d7a8a1a34dbdeefe5a3523d90fb72894390
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 02:13:20 +0600

    Commit Wed 10/22/2025  2:13:20.24 11183

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -33,12 +33,7 @@ class APIController extends Controller
     {
         $districts = District::where('division_id', $divisionId)->pluck('name', 'id');
         $districts = Cache::remember('districtsapi', 10 * 24 * 60 * 60, function () use ($divisionId) {
-            $districts = District::where('division_id', $divisionId)
-                                 ->orderBy('exam_id', 'desc')
-                                 // ->join('exams', 'exams.id', '=', 'districts.exam_id')
-                                 // ->orderBy('exams.available_from', 'asc')
-                                 // ->select('districts.*')
-                                 ->get();
+            $districts = District::where('division_id', $divisionId)->pluck('name', 'id');
 
             foreach($districts as $courseexam) {
                 $courseexam->name = $courseexam->exam->name;
