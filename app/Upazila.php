2a59296b277e375a63bcacabfc6d090dbd197703
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 21:57:03 +0600

    Commit Tue 10/21/2025 21:57:03.21 8464

--- app/Upazila.php
+++ app/Upazila.php
@@ -1,10 +1,41 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Upazila extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'district_id',
+        'name',
+        'bn_name',
+        'url',
+    ];
+
+    /**
+     * Get the district that owns the upazila.
+     */
+    public function district(): BelongsTo
+    {
+        return $this->belongsTo(District::class);
+    }
+
+    /**
+     * Get the unions for the upazila.
+     */
+    public function unions(): HasMany
+    {
+        return $this->hasMany(Union::class);
+    }
 }
