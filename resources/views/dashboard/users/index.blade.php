76c290390016894ddacfba3c3c7a2e2a5957f4b0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 00:37:28 +0600

    Commit Wed 10/22/2025  0:37:28.26 29229

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -310,7 +310,7 @@
     {{-- Add User Modal Code --}}
     <!-- Modal -->
     <div class="modal fade" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="addUserModalLabel" aria-hidden="true" data-backdrop="static">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header bg-success">
             <h5 class="modal-title" id="addUserModalLabel">নতুন ব্যবহারকারী যোগ</h5>
