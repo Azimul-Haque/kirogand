7ebb1a90eb6bf7b26f16c57134e2a6ec6418e196
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 01:53:22 +0600

    Commit Wed 10/22/2025  1:53:22.29 17327

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -350,7 +350,7 @@
 	                	<select name="role" id="adduserrole" class="form-control" required>
 	                		<option selected="" disabled="" value="">ধরন</option>
 	                		<option value="admin">এডমিন</option>
-							       <option value="manager">ম্যানেজার</option>
+							       <option value="manager">স্থানীয় সরকার প্রতিনিধি</option>
                      <option value="volunteer">ভলান্টিয়ার</option>
 	                		<option value="user">ব্যবহারকারী</option>
 							{{-- <option value="accountant">একাউন্টেন্ট</option> --}}
