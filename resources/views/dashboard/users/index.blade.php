84e3f43a1bc8304fef53101942ac4ebec5bc4732
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 03:50:06 +0600

    Commit Wed 10/22/2025  3:50:06.27 28064

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -62,7 +62,7 @@
                     <td>
 
                       @if ($user->authorities->isNotEmpty())
-                          {{ print_r($user->authorities->first()->getAncestorsByLevel()) }}
+                          {{-- {{ print_r($user->authorities->first()->getAncestorsByLevel()) }} --}}
                           @php
                               $auth = $user->authorities->first();
                           @endphp
