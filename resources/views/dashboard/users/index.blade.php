881b1cb733155d563851fb55c8ac17279b0e4288
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 01:58:04 +0600

    Commit Wed 10/22/2025  1:58:04.32 27134

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -208,7 +208,7 @@
             				                	<select name="role" class="form-control" required>
             				                		<option disabled="" value="">ধরন নির্ধারণ করুন</option>
             				                		<option value="admin" @if($user->role == 'admin') selected="" @endif>এডমিন</option>
-              													<option value="manager" @if($user->role == 'manager') selected="" @endif>ম্যানেজার</option>
+              													<option value="manager" @if($user->role == 'manager') selected="" @endif>স্থানীয় সরকার প্রতিনিধি</option>
                                         <option value="volunteer" @if($user->role == 'volunteer') selected="" @endif>ভলান্টিয়ার</option>
               													<option value="user" @if($user->role == 'user') selected="" @endif>ব্যবহারকারী</option>
               													{{-- <option value="accountant" @if($user->role == 'accountant') selected="" @endif>একাউন্টেন্ট</option> --}}
