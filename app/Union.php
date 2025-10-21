ffa2c18bf7b3129474f3f84b5b3446cd01d69285
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 21:57:23 +0600

    Commit Tue 10/21/2025 21:57:23.24 11638

--- app/Union.php
+++ app/Union.php
@@ -1,10 +1,32 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Union extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'upazila_id',
+        'name',
+        'bn_name',
+        'url',
+    ];
+
+    /**
+     * Get the upazila that owns the union.
+     */
+    public function upazila(): BelongsTo
+    {
+        return $this->belongsTo(Upazila::class);
+    }
 }
