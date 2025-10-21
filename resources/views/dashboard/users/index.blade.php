b5164eed5a189766cafccecdbc5c203dd1e45429
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 01:55:11 +0600

    Commit Wed 10/22/2025  1:55:11.26 18723

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -367,7 +367,7 @@
                              class="form-control"
                              placeholder="পদবি (প্রশাসক/মেয়র/চেয়ারম্যান/সচিব ইত্যাদি)">
                       <div class="input-group-append">
-                          <div class="input-group-text"><span class="fas fa-phone"></span></div>
+                          <div class="input-group-text"><span class="fas fa-user-secret"></span></div>
                       </div>
                   </div>
 
