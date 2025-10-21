bf358dff47c003a62aec161523fb4f76a93ac647
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 21:58:52 +0600

    Commit Tue 10/21/2025 21:58:52.32 15924

--- database/migrations/2025_10_21_215616_create_districts_table.php
+++ database/migrations/2025_10_21_215616_create_districts_table.php
@@ -15,7 +15,18 @@ class CreateDistrictsTable extends Migration
     {
         Schema::create('districts', function (Blueprint $table) {
             $table->id();
+            // Foreign key to Division table
+            $table->foreignId('division_id')->constrained()->onDelete('cascade');
+
+            $table->string('name');
+            $table->string('bn_name');
+            $table->decimal('lat', 10, 8)->nullable(); // Latitude
+            $table->decimal('lon', 11, 8)->nullable(); // Longitude
+            $table->string('url')->nullable();
             $table->timestamps();
+
+            // Ensure district names are unique within a division
+            $table->unique(['division_id', 'name']);
         });
     }
 
