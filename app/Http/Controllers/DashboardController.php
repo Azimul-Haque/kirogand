1ffa7f742277df66026e6aedbf60617a82bc9f18
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 02:27:47 +0600

    Commit Wed 10/22/2025  2:27:47.31 6810

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -321,8 +321,8 @@ class DashboardController extends Controller
         $this->validate($request,array(
             'name'        => 'required|string|max:191',
             'mobile'      => 'required|string|max:191|unique:users,mobile,'.$id,
-            'role'        => 'required',
             'designation'        => 'sometimes',
+            'role'        => 'required',
             'packageexpirydate'        => 'required',
             'uid'        => 'sometimes',
             'onesignal_id'        => 'sometimes',
