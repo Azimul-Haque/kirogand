dab63dd63c24f5c2f436ef2212611b4b98bc8a5b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-22 03:17:12 +0600

    Commit Wed 10/22/2025  3:17:12.31 28647

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -294,8 +294,8 @@
                                   <h6 class="mb-3">কর্তৃপক্ষ (Authority) নির্ধারণ (ঐচ্ছিক)</h6>
                                   
                                   <!-- DYNAMIC AUTHORITY FIELDS FOR EDIT -->
-                                  <input type="hidden" name="authority_level" id="edit_authority_level">
-                                  <input type="hidden" name="authority_id" id="edit_authority_id">
+                                  <input type="hidden" name="authority_level" id="edit_authority_level{{ $user->id }}">
+                                  <input type="hidden" name="authority_id" id="edit_authority_id{{ $user->id }}">
 
                                   <!-- Division Dropdown (Level 1) -->
                                   <div class="input-group mb-3">
