9d64c1a7d9e80a7303bc619ca643a295957db8b3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-21 22:55:29 +0600

    Commit Tue 10/21/2025 22:55:29.27 23379

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -23,9 +23,9 @@
                 <button type="button" id="search-button" class="btn btn-default btn-sm" style="margin-left: 5px;">
                   <i class="fas fa-search"></i> খুঁজুন
                 </button>
-                <button type="button" class="btn btn-info btn-sm"  data-toggle="modal" data-target="#addBulkDate" style="margin-left: 5px;">
+                {{-- <button type="button" class="btn btn-info btn-sm"  data-toggle="modal" data-target="#addBulkDate" style="margin-left: 5px;">
                   <i class="fas fa-calendar-alt"></i> বাল্ক মেয়াদ বাড়ান
-                </button>
+                </button> --}}
                 <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addUserModal" style="margin-left: 5px;">
                   <i class="fas fa-user-plus"></i> নতুন
                 </button>
