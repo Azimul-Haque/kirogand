499318497c8040d3603bd872dd222f8ec046fffc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-23 02:50:26 +0600

    Commit Thu 10/23/2025  2:50:26.29 26791

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'IndexController@index')->name('index.index');
 Route::get('/services', 'IndexController@getServices')->name('index.services');
-Route::get('/verify-certificate', 'IndexController@verifyCertificate')->name('index.verify-certificate');
+Route::get('/verify-certificate', 'IndexController@getVerifyCertificate')->name('index.verify-certificate');
 Route::get('/application-status', 'IndexController@applicationStatus')->name('index.application-status');
 Route::get('/notices', 'IndexController@getNotices')->name('index.notices');
 
