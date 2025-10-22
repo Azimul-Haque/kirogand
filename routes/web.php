11f610f975365c4015ed0659ab2cbe26d2642a4f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-23 04:11:06 +0600

    Commit Thu 10/23/2025  4:11:06.28 27631

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,9 @@ Route::get('bkash/success/page/web', 'BkashController@bkashSuccessPageWeb')->nam
 Route::get('bkash/failed/page/web', 'BkashController@bkashFailedPageWeb')->name('bkash-failed-page-web');
 // Payment Routes for bKash
 
-Auth::routes('register' => false,);
+Auth::routes([
+    'register' => false,
+]);
 
 Route::get('/testunion', 'DashboardController@getDivDUUniData')->name('dashboard.testunion');
 
