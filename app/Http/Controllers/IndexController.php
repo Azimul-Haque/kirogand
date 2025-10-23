0682915c5b1f698f0d023ee1337430125cdbba13
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 01:26:16 +0600

    Commit Fri 10/24/2025  1:26:16.29 11355

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -330,7 +330,7 @@ class IndexController extends Controller
         return view('index.auth.register-authority');
     }
 
-    public function storeUser(Request $request)
+    public function storeAuthorityRegister(Request $request)
     {
         $this->validate($request,array(
             'name'        => 'required|string|max:191',
