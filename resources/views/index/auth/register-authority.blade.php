20c34ff8265f36c58452ae848b58110a051a826c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 01:40:13 +0600

    Commit Fri 10/24/2025  1:40:13.21 3571

--- resources/views/index/auth/register-authority.blade.php
+++ resources/views/index/auth/register-authority.blade.php
@@ -143,6 +143,12 @@
                                   <!-- Options based on selected Division in real app -->
                               </select>
                           </div>
+
+                          <div class="input-group mb-3">
+                              <select id="add_district_id" class="form-control authority-select" data-level="District" data-target="add_upazila_id" data-model="Upazila" disabled>
+                                  <option value="" selected disabled>জেলা নির্বাচন করুন</option>
+                              </select>
+                          </div>
                           
                           <!-- থানা / উপজেলা -->
                           <div class="col-md-6">
