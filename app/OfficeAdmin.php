24f8409491a6a97a5bd51c6eec982c0f05a24658
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 00:26:34 +0600

    Commit Fri 10/24/2025  0:26:34.25 30493

--- app/OfficeAdmin.php
+++ app/OfficeAdmin.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class OfficeAdmin extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'office_admins';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'local_office_id',
+        'user_id',
+        'role',
+    ];
+
+    /**
+     * Get the office associated with this administrator.
+     */
+    public function localOffice()
+    {
+        return $this->belongsTo(LocalOffice::class);
+    }
+
+    /**
+     * Get the user (admin) associated with this relationship.
+     */
+    public function user()
+    {
+        // Assuming your main user model is named 'User'
+        return $this->belongsTo(User::class); 
+    }
 }
