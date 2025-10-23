5d96a22d773173b4b7b0d5b00e90a1362292c21f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 00:27:47 +0600

    Commit Fri 10/24/2025  0:27:47.27 4986

--- app/LocalOffice.php
+++ app/LocalOffice.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class LocalOffice extends Model
 {
-    //
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'local_offices';
+
+    /**
+    * The attributes that are mass assignable.
+    * The geographical fields are simplified to one.
+    *
+    * @var array
+    */
+    protected $fillable = [
+    'authority_name_bn',
+    'authority_type',
+    'is_active',
+
+    // Contact and Visuals
+    'email',
+    'phone',
+    'monogram',
+
+    // Geographical Field (Single Anchor)
+    'geo_location_id', 
+    ];
+
+    /**
+    * Get the administrators (users) associated with this Local Office.
+    */
+    public function admins()
+    {
+    return $this->hasMany(OfficeAdmin::class);
+    }
 }
