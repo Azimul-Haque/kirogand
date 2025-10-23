afc278da4c1c21179347ba157d34f07a5f0c0f3f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 00:46:11 +0600

    Commit Fri 10/24/2025  0:46:11.27 2966

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('local_office_id')->nullable();
             $table->string('uid')->nullable();
             $table->string('onesignal_id')->nullable();
             $table->string('name');
