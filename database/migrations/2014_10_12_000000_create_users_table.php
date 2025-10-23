0838245cb0c7adff0b43d143db198615c2a4681c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 01:04:34 +0600

    Commit Fri 10/24/2025  1:04:34.25 120

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('uid')->nullable();
             $table->string('onesignal_id')->nullable();
             $table->string('name');
+            $table->string('name_en')->nullable();
             $table->string('role')->default('user');
             $table->string('deignation')->nullable();
             $table->string('mobile')->unique();
