2f50e76fa5bfd5b4cb94afae04efa6315e4392fe
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 16:38:56 +0600

    Commit Fri 10/24/2025 16:38:56.24 11563

--- resources/views/index/auth/logged_in_inactive_page.blade.php
+++ resources/views/index/auth/logged_in_inactive_page.blade.php
@@ -46,7 +46,7 @@
       </h1>
       <p class="text-center lead mb-5 text-muted">স্থানীয় সরকার কর্তৃপক্ষ (ইউনিয়ন/পৌরসভা/উপজেলা পরিষদ/জেলা পরিষদ) ও কর্মকর্তা একাউন্ট নিবন্ধন</p>
 
-      <div id="resultContainer" class="mt-5 d-none">
+      <div id="resultContainer" class="mt-5">
           <h2 class="h3 fw-bold mb-4 text-center" style="color: var(--darker-color);">যাচাইয়ের ফলাফল</h2>
           <div class="row justify-content-center">
               <div class="col-md-8 col-lg-6">
