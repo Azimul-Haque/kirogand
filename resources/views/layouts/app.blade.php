0683d92438a1654d2690007373d06e5a8961fda7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 17:51:22 +0600

    Commit Fri 10/24/2025 17:51:22.27 14617

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,8 +111,8 @@
                         <p>
                             {{ Auth::user()->name }}<br/>
                             @if(Auth::user()->role == 'manager')
-                            <span style="font-size: 14px; padding-top: -20px;">{{ Auth::user()->designation }}</span><br/>
-                            <span style="font-size: 14px; padding-top: -20px;">{{ Auth::user()->localOffice->name_bn }}</span><br/>
+                                <span style="font-size: 14px; padding-top: -20px;">{{ Auth::user()->designation }}</span><br/>
+                                <span style="font-size: 14px; padding-top: -20px;">{{ Auth::user()->localOffice->name_bn }}</span><br/>
                             @endif
                             {{-- <small>যোগদানঃ {{ bangla(Auth::user()->created_at->format('F Y')) }}</small> --}}
                         </p>
