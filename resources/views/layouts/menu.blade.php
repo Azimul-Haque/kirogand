facf87e4f18c16bdb49032dc2939c9a2372e79c6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-25 00:21:20 +0600

    Commit Sat 10/25/2025  0:21:20.28 22703

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -28,7 +28,7 @@
 <li class="nav-item">
     <a href="{{ route('dashboard.local-offices') }}" class="nav-link {{ Request::is('dashboard/local-offices') ? 'active' : '' }} {{ Request::is('dashboard/local-offices/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-landmark"></i>
-        <p>ইউনিয়ন/পৌরসভা তালিকা</p>
+        <p>সনদের আবেদনসমূহ</p>
     </a>
 </li>
 @endif
