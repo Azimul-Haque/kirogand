e98237a42a669053e25016c273a6bf04aaa490db
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-25 00:27:25 +0600

    Commit Sat 10/25/2025  0:27:25.27 28703

--- resources/views/dashboard/apply-for-certificate/index.blade.php
+++ resources/views/dashboard/apply-for-certificate/index.blade.php
@@ -12,70 +12,7 @@
 
 @section('content')
   @section('page-header') সনদের আবেদন @endsection
-    <div class="container-fluid">
-    <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">সনদের আবেদন তালিকা (মোট: )</h3>
-
-            <div class="card-tools">
-              {{-- <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addPackageModal" title="" rel="tooltip" data-original-title="সনদের আবেদন যোগ করুন">
-                <i class="fas fa-clipboard-check"></i> নতুন সনদের আবেদন
-              </button> --}}
-              <div class="card-tools">
-                <form class="form-inline form-group-lg" action="">
-                  <div class="form-group">
-                    <input type="search-param" class="form-control form-control-sm" placeholder="সনদের আবেদন খুঁজুন" id="search-param" required>
-                  </div>
-                  <button type="button" id="search-button" class="btn btn-default btn-sm" style="margin-left: 5px;">
-                    <i class="fas fa-search"></i> খুঁজুন
-                  </button>
-                </form>
-                
-              </div>
-            </div>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body p-0">
-            <table class="table">
-              <thead>
-                <thead>
-                  <th>কার্যালয়</th>
-                  <th>অবস্থা</th>
-                  <th>ধরন</th>
-                  <th>ব্যবহারকারীগণ</th>
-                  <th>যোগদান</th>
-                  <th>একশন</th>
-                </thead>
-              </thead>
-              <tbody>
-                @foreach($localoffices as $localoffice)
-                  <tr>
-                    <td>
-                      {{ $localoffice->name_bn }}</br>
-                      <span class="badge badge-primary">📞 {{ $localoffice->phone }}</span>
-                      <span class="badge badge-warning">✉ {{ $localoffice->email }}</span>
-                    </td>
-                    <td><span class="badge badge-{{ $localoffice->is_active == 0 ? 'light' : 'success' }}">{{ $localoffice->is_active == 0 ? 'এক্টিভ নয়' : 'একটিভ' }}</span></td>
-                    <td>{{ $localoffice->office_type == 'up' ? 'ইউনিয়ন পরিষদ' : 'পৌরসভা' }}</td>
-                    <td>
-                      @foreach($localoffice->users as $user)
-                        {{ $user->name }}
-                      @endforeach
-                    </td>
-                    <td>{{ date('F d, Y', strtotime($localoffice->created_at)) }}</td>
-                    <td>
-
-                    </td>
-                  </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
-          <!-- /.card-body -->
-        </div>
-        {{ $localoffices->links() }}
-
-    </div>
+    
 @endsection
 
 @section('third_party_scripts')
