6e30a156e55916002a38836f2dd1a3d856f9a784
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 16:40:30 +0600

    Commit Fri 10/24/2025 16:40:30.26 7306

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ Auth::routes([
 
 Route::get('register/authority', 'IndexController@getAuthorityRegister')->name('register.authority');
 Route::post('register/authority', 'IndexController@storeAuthorityRegister')->name('register.store.authority');
-Route::get('register/authority/message', 'IndexController@storeAuthorityRegister')->name('register.authority.message');
+// Route::get('register/authority/message', 'IndexController@storeAuthorityRegister')->name('register.authority.message');
 Route::get('register/citizen', 'IndexController@getCitizenRegister')->name('register.citizen');
 
 Route::get('/testunion', 'DashboardController@getDivDUUniData')->name('dashboard.testunion');
