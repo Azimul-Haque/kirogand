f6764d9d0ca5d2fcc33c3133a58b0d2f40c7c3ce
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-24 22:29:24 +0600

    Commit Fri 10/24/2025 22:29:24.28 14091

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') ড্যাশবোর্ড | পেমেন্ট @endsection
+@section('title') ড্যাশবোর্ড | ইউনিয়ন/পৌরসভা @endsection
 
 @section('third_party_stylesheets')
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
@@ -11,20 +11,20 @@
 @endsection
 
 @section('content')
-  @section('page-header') পেমেন্ট @endsection
+  @section('page-header') ইউনিয়ন/পৌরসভা @endsection
     <div class="container-fluid">
     <div class="card">
           <div class="card-header">
-            <h3 class="card-title">পেমেন্ট তালিকা</h3>
+            <h3 class="card-title">ইউনিয়ন/পৌরসভা তালিকা</h3>
 
             <div class="card-tools">
-              {{-- <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addPackageModal" title="" rel="tooltip" data-original-title="পেমেন্ট যোগ করুন">
-                <i class="fas fa-clipboard-check"></i> নতুন পেমেন্ট
+              {{-- <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addPackageModal" title="" rel="tooltip" data-original-title="ইউনিয়ন/পৌরসভা যোগ করুন">
+                <i class="fas fa-clipboard-check"></i> নতুন ইউনিয়ন/পৌরসভা
               </button> --}}
               <div class="card-tools">
                 <form class="form-inline form-group-lg" action="">
                   <div class="form-group">
-                    <input type="search-param" class="form-control form-control-sm" placeholder="পেমেন্ট খুঁজুন" id="search-param" required>
+                    <input type="search-param" class="form-control form-control-sm" placeholder="ইউনিয়ন/পৌরসভা খুঁজুন" id="search-param" required>
                   </div>
                   <button type="button" id="search-button" class="btn btn-default btn-sm" style="margin-left: 5px;">
                     <i class="fas fa-search"></i> খুঁজুন
@@ -41,7 +41,7 @@
                 <thead>
                   <th>ব্যবহারকারী</th>
                   <th>প্যাকেজ</th>
-                  <th>পেমেন্ট স্ট্যাটাস</th>
+                  <th>ইউনিয়ন/পৌরসভা স্ট্যাটাস</th>
                   <th>কার্ডের ধরন</th>
                   <th>ট্রানজেকশন আইডি</th>
                   <th>পরিমাণ</th>
