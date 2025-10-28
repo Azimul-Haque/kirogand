4ecabd93caebef29d840ea5168aaf7bbc92868d2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-29 01:52:55 +0600

    Commit Wed 10/29/2025  1:52:55.22 5537

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -261,7 +261,7 @@
                      aria-expanded="false" 
                      style="padding: 6px 15px;">
                      @if(Auth::user())
-                      <i class="fas fa-sign-in-alt me-1"></i> লগইন
+                      <i class="fas fa-sign-in-alt me-1"></i> {{ Auth::user()->name }}
                      @else
                       <i class="fas fa-sign-in-alt me-1"></i> লগইন
                      @endif
