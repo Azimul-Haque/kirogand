040062437507d79b8b5ae55e38c03c6cef75bd94
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-29 00:45:47 +0600

    Commit Wed 10/29/2025  0:45:47.23 12922

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -200,7 +200,7 @@
 
       <!-- 2. Logo/Branding Area (Bigger Logo) -->
       <div class="logo-area">
-          <div class="container py-3 d-flex align-items-center justify-content-between">
+          <div class="container py-3 d-flex align-items-center justify-content-between sticky-top">
               <div class="d-flex align-items-center">
                   <!-- Increased Icon Size and Text Emphasis -->
                   {{-- <i class="fas fa-landmark me-3" style="font-size: 2.8rem; color: var(--light-primary-color);"></i> --}}
