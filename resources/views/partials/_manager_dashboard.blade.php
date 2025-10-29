1b904eec979d92d905f1a6a1ba43fcc6b1e8a0b0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 05:06:03 +0600

    Commit Thu 10/30/2025  5:06:03.22 24980

--- resources/views/partials/_manager_dashboard.blade.php
+++ resources/views/partials/_manager_dashboard.blade.php
@@ -79,11 +79,10 @@
                 </h3>
             </div>
             <div class="card-body">
-                <div class="tab-content p-0">
+                
                     <div class="chart">
                       <canvas id="lineChart2" style="min-height: 250px; height: 300px; max-height: 400px; max-width: 100%;"></canvas>
                     </div>
-                </div>
             </div>
         </div>
     </section>
