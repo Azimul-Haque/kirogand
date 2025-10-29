aeac7ccca567185e107c3bd6eadc1e332164e3a3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:19:40 +0600

    Commit Thu 10/30/2025  4:19:40.27 8801

--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -181,7 +181,7 @@
                                     <input type="text"
                                            name="name"
                                            class="form-control"
-                                           value="{{ Auth::user()->localOffice->name_bn }}"
+                                           value="{{ Auth::user()->localOffice->name }}"
                                            placeholder="ইংরেজি নাম (OPTIONAL)">
                                     <div class="input-group-append">
                                         <div class="input-group-text"><span class="fas fa-flag"></span></div>
