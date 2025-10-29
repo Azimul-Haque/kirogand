794088a07269dc22ade89753a1454cf02ef29c98
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:57:58 +0600

    Commit Thu 10/30/2025  4:57:58.26 27081

--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -13,6 +13,12 @@
 
 @section('content')
   @section('page-header') প্রোফাইল @endsection
+  @section('page-header-right')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
+        <li class="breadcrumb-item active">ইউনিয়ন/পৌরসভা</li>
+    </ol>
+  @endsection
     <div class="container-fluid">
       <div class="row">
         <!-- ============================================= -->
