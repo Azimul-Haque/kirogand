eacd11544ba4e247ae3fd9129f333c29a3e327f4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:59:16 +0600

    Commit Thu 10/30/2025  4:59:16.31 10951

--- resources/views/dashboard/notifications/index.blade.php
+++ resources/views/dashboard/notifications/index.blade.php
@@ -8,6 +8,12 @@
 
 @section('content')
 	@section('page-header') নোটিফিকেশন @endsection
+  @section('page-header-right')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
+        <li class="breadcrumb-item active">প্যাকেজ</li>
+    </ol>
+  @endsection
     <div class="container-fluid">
   		<div class="row">
         <div class="col-md-9">
