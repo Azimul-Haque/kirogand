6763c631011a15a310f35ae6f7a9bffc00a712c9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 03:30:14 +0600

    Commit Thu 10/30/2025  3:30:14.28 21674

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -402,15 +402,6 @@
                                     </div>
                                   </div>
 
-                                  <div class="row">
-                                    <div class="col-md-6">
-                                      
-                                    </div>
-                                    <div class="col-md-6">
-                                      
-                                    </div>
-                                  </div>
-
                                   {{-- <div class="input-group mb-3">
                                       <input type="text"
                                              name="packageexpirydate"
