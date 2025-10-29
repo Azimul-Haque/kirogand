85e07a444622c8f542283f714a90b4935acb8a49
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-29 23:58:29 +0600

    Commit Wed 10/29/2025 23:58:29.28 16335

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -65,10 +65,10 @@
                     </td>
                     <td>
 
-                      @if ($user->authority->isNotEmpty())
-                          {{-- {{ print_r($user->authority->first()->getAncestorsByLevel()) }} --}}
+                      @if ($user->authorities->isNotEmpty())
+                          {{-- {{ print_r($user->authorities->first()->getAncestorsByLevel()) }} --}}
                           @php
-                              $auth = $user->authority->first();
+                              $auth = $user->authorities->first();
                           @endphp
                           {{-- Display the full dynamic hierarchy string --}}
                           {{-- {!! $auth->getFullHierarchy() !!}  --}}
@@ -77,7 +77,46 @@
                           </span>
                       @endif
 
-                      
+                      @php
+                        $userAuthority = $user->authorities->first();
+
+                        if ($userAuthority) {
+                          // 2. Call the method to get the full hierarchy array
+                          $hierarchy = $userAuthority->getAncestorsByLevel();
+
+                          // 3. Access the specific level you need (e.g., 'District')
+                          // Note: The keys are the English model names: 'Division', 'District', 'Upazila', 'Union'
+                          
+                          // To get the Division Model:
+                          $divisionModel = $hierarchy['Division'] ?? null;
+
+                          // To get the District Model:
+                          $districtModel = $hierarchy['District'] ?? null;
+
+                          // To get the Upazilla Model:
+                          $upazilaModel = $hierarchy['Upazila'] ?? null;
+                          
+                          // To get the Assigned Authority Model (Union, Upazila, etc.):
+                          $assignedModel = $userAuthority->authority; 
+                          $level = (new \ReflectionClass($userAuthority->authority_type))->getShortName();
+                          
+                          // --- Displaying Data ---
+                          if ($districtModel) {
+                              // You can now access any column (ID, name, bn_name)
+                              echo "<br/>বিভাগ: " . $divisionModel->bn_name;
+                              echo ", জেলা: " . $districtModel->bn_name;
+                              // echo "<br/>District ID: " . $districtModel->id;
+                          }
+
+                          if ($upazilaModel) {
+                              // You can now access any column (ID, name, bn_name)
+                              echo "<br/>উপজেলা: " . $upazilaModel->bn_name;
+                              if($level == 'Union') {
+                                echo ", ইউনিয়ন: " . $assignedModel->bn_name;
+                              }
+                          }
+                      }
+                      @endphp
                     </td>
                 		<td align="right" width="15%">
                       {{-- <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#smsModal{{ $user->id }}">
