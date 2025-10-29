f694c89ea718de0ba33a51a8ca179b6ceb44c7dd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:59:44 +0600

    Commit Thu 10/30/2025  4:59:44.23 31544

--- resources/views/dashboard/messages/index.blade.php
+++ resources/views/dashboard/messages/index.blade.php
@@ -7,6 +7,12 @@
 
 @section('content')
 	@section('page-header') মেসেজসমূহ @endsection
+  @section('page-header-right')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
+        <li class="breadcrumb-item active">ব্লগসমূহ</li>
+    </ol>
+  @endsection
     <div class="container-fluid">
 		<div class="card">
           <div class="card-header">
