c69a80628c4ae4dcacd1a2370200aeea6df81409
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 00:14:57 +0600

    Commit Thu 10/30/2025  0:14:57.31 23351

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -80,7 +80,7 @@
                     <div class="modal-dialog modal-lg" role="document">
                       <div class="modal-content">
                         <div class="modal-header bg-primary">
-                          <h5 class="modal-title" id="editLocalOfficeModalLabel">ব্যবহারকারী তথ্য হালনাগাদ</h5>
+                          <h5 class="modal-title" id="editLocalOfficeModalLabel">স্থানীয় সরকার কার্যালয় তথ্য হালনাগাদ</h5>
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                           </button>
