a4faf010f0a718ece3fd11adfeff243d9264fc7a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-29 23:41:39 +0600

    Commit Wed 10/29/2025 23:41:39.22 28238

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -68,7 +68,7 @@
                     <td><small>{{ date('F d, Y', strtotime($localoffice->created_at)) }}</small></td>
                     <td>
                       <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editUserModal{{ $localoffice->id }}">
-                        <i class="fas fa-user-edit"></i>
+                        <i class="fas fa-office"></i>
                       </button>
                     </td>
                   </tr>
