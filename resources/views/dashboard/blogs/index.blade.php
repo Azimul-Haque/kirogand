4f4df98ca5f11e09b4c1d6baa81879c7f09647a7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:59:30 +0600

    Commit Thu 10/30/2025  4:59:30.29 14861

--- resources/views/dashboard/blogs/index.blade.php
+++ resources/views/dashboard/blogs/index.blade.php
@@ -22,6 +22,12 @@
 
 @section('content')
     @section('page-header') ব্লগসমূহ @endsection
+    @section('page-header-right')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
+        <li class="breadcrumb-item active">ব্লগসমূহ</li>
+    </ol>
+  @endsection
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-9">
