9f00e19e5c6ab0e75873225e414c2f6c67acee90
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-29 23:59:07 +0600

    Commit Wed 10/29/2025 23:59:07.31 27843

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -129,6 +129,18 @@
                                 }
                             }
                             @endphp
+
+                            @if ($user->authorities->isNotEmpty())
+                                {{-- {{ print_r($user->authorities->first()->getAncestorsByLevel()) }} --}}
+                                @php
+                                    $auth = $user->authorities->first();
+                                @endphp
+                                {{-- Display the full dynamic hierarchy string --}}
+                                {{-- {!! $auth->getFullHierarchy() !!}  --}}
+                                <span class="badge badge-secondary">
+                                    ({{ (new \ReflectionClass($auth->authority_type))->getShortName() }})
+                                </span>
+                            @endif
                             
                                 @csrf
 
