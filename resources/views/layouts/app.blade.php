1928a6f36b0d4f21ad0d5c898bf4b28adceb5b35
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 03:39:45 +0600

    Commit Thu 10/30/2025  3:39:45.29 30235

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,7 +119,7 @@
                     </li>
                     <!-- Menu Footer-->
                     <li class="user-footer">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
+                        <a href="{{ route('dashboard.profile') }}" class="btn btn-default btn-flat">Profile</a>
                         <a href="#" class="btn btn-default btn-flat float-right"
                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             লগআউট
