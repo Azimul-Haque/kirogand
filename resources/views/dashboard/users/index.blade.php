9dec1dcf39187a74ad1d9102abd19a5ef5a9a91e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-29 23:28:47 +0600

    Commit Wed 10/29/2025 23:28:47.22 29986

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -60,7 +60,7 @@
                       <small><span>প্যাকেজ: <b>{{ date('d F, Y', strtotime($user->package_expiry_date)) }}</b></span></small>
                 		</td>
                     <td>
-                      
+                      {{ $user->designation }}
                     </td>
                     <td>
 
