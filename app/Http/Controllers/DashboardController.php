dd29945b8bd402a8a75658621b76eeaf96c8fcd4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 03:16:15 +0600

    Commit Thu 10/30/2025  3:16:15.21 3772

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -407,6 +407,7 @@ class DashboardController extends Controller
     {
         $this->validate($request,array(
             'name'        => 'required|string|max:191',
+            'name_en'        => 'required|string|max:191',
             'mobile'      => 'required|string|max:191|unique:users,mobile,'.$id,
             'designation'        => 'sometimes',
             'role'        => 'required',
