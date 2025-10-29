17ee5a7f7735298b9332c0d606733f82363ba909
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 00:14:07 +0600

    Commit Thu 10/30/2025  0:14:07.28 22357

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -101,6 +101,11 @@
                             
                                 @csrf
 
+                                <div class="custom-control custom-switch">
+                                  <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                                  <label class="custom-control-label" for="customSwitch1">Toggle this switch element</label>
+                                </div>
+                                
                                 <div class="input-group mb-3">
                                     <input type="text"
                                            name="name_bn"
