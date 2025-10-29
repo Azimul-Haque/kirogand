6f76c082cbb0b679ef010f3a6072d32e8cbd2144
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:23:50 +0600

    Commit Thu 10/30/2025  4:23:50.23 13360

--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -241,7 +241,7 @@
                               @php
                                   // 1. Define the full local path to the file.
                                   // We use public_path() because that's where File::exists() looks.
-                                  $imagePath = public_path('images/localoffices/' . $localoffice->monogram);
+                                  $imagePath = public_path('images/localoffices/' . Auth::user()->localoffice->monogram);
 
                                   // 2. Check if the file name is stored AND if the physical file exists.
                                   $monogramExists = $localoffice->monogram && File::exists($imagePath);
