c02a50c8d9e939347abdbcf3f3be7e113433fb16
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:57:44 +0600

    Commit Thu 10/30/2025  4:57:44.32 12161

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -13,6 +13,12 @@
 
 @section('content')
   @section('page-header') ইউনিয়ন/পৌরসভা @endsection
+  @section('page-header-right')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
+        <li class="breadcrumb-item active">ব্যবহারকারীগণ</li>
+    </ol>
+  @endsection
     <div class="container-fluid">
       <div class="card">
           <div class="card-header">
