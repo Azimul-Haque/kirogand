32aeb9852c42c3140a155a7f596e7a42e74726e6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:20:46 +0600

    Commit Thu 10/30/2025  4:20:46.25 17103

--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -228,11 +228,11 @@
                                     <div class="custom-file">
                                         <input type="file"
                                                class="custom-file-input"
-                                               id="monogram_file"
+                                               id="monogram"
                                                name="monogram"
                                                accept="image/png, image/jpeg, image/gif"
                                                required> <!-- Assuming monogram is null, hence required -->
-                                        <label class="custom-file-label" for="monogram_file">মনোগ্রাম সিলেক্ট করুন</label>
+                                        <label class="custom-file-label" for="monogram">মনোগ্রাম সিলেক্ট করুন</label>
                                     </div>
                                     <small class="form-text text-muted">সর্বোচ্চ ফাইল সাইজ 300KB। (PNG, JPG, GIF)</small>
                                 </div>
