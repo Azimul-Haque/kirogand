fa1b08699c6d3011f64ecd084b1992a20d1f3318
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 00:25:26 +0600

    Commit Thu 10/30/2025  0:25:26.26 10521

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -133,11 +133,9 @@
                                   </div>
                                 </div>
 
-                                
-
-                                
-
-                                <div class="input-group mb-3">
+                                <div class="row">
+                                  <div class="col-md-6">
+                                    <div class="input-group mb-3">
                                     <input type="text"
                                            name="mobile"
                                            value="{{ $localoffice->mobile }}"
@@ -148,6 +146,26 @@
                                         <div class="input-group-text"><span class="fas fa-phone"></span></div>
                                     </div>
                                 </div>
+                                  </div>
+                                  <div class="col-md-6">
+                                    <div class="input-group mb-3">
+                                        <input type="text"
+                                               name="name"
+                                               class="form-control"
+                                               value="{{ $localoffice->name }}"
+                                               placeholder="ইংরেজি নাম (OPTIONAL)">
+                                        <div class="input-group-append">
+                                            <div class="input-group-text"><span class="fas fa-user"></span></div>
+                                        </div>
+                                    </div>
+                                  </div>
+                                </div>
+
+                                
+
+                                
+
+                                
 
                                 <div class="input-group mb-3">
                                     <input type="email"
