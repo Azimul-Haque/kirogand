5191175cfa2df4f6dea286991bdd2760ed2258be
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:55:05 +0600

    Commit Thu 10/30/2025  4:55:05.25 3501

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -8,6 +8,12 @@
 
 @section('content')
 	@section('page-header') ব্যবহারকারীগণ (মোট {{ bangla($userscount) }} জন) @endsection
+  @section('page-header-right')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
+        <li class="breadcrumb-item active">ড্যাশবোর্ড</li>
+    </ol>
+  @endsection
     <div class="container-fluid">
 		  <div class="card">
           <div class="card-header">
