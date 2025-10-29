5752ab4b7878265632e0ea3cf4151d5eccc2d51e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 04:59:09 +0600

    Commit Thu 10/30/2025  4:59:09.30 29460

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -7,6 +7,12 @@
 
 @section('content')
 	@section('page-header') প্যাকেজ @endsection
+  @section('page-header-right')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
+        <li class="breadcrumb-item active">প্যাকেজ</li>
+    </ol>
+  @endsection
     <div class="container-fluid">
 		<div class="card">
           <div class="card-header">
