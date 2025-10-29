720d2d7b8a3e21dccfa7943b196bf89750aaf9d2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-30 03:10:20 +0600

    Commit Thu 10/30/2025  3:10:20.23 28859

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -379,6 +379,9 @@
 
                                   <div class="row">
                                     <div class="col-md-6">
+                                      
+                                    </div>
+                                    <div class="col-md-6">
                                       <div class="input-group mb-3">
                                           <input type="password"
                                                  name="password"
@@ -389,9 +392,6 @@
                                               <div class="input-group-text"><span class="fas fa-lock"></span></div>
                                           </div>
                                       </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                      
                                     </div>
                                   </div>
 
