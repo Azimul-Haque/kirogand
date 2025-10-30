53a053e950aeaf63ec0d069407399dd633fbdbe5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 04:21:20 +0600

    Commit Fri 10/31/2025  4:21:20.31 3640

--- app/Http/Controllers/BkashController.php
+++ app/Http/Controllers/BkashController.php
@@ -171,7 +171,6 @@ class BkashController extends Controller
 
     public function bkashSuccess(Request $request)
     {
-        
         $user = User::where('mobile', $request->mobile)->first();
         
         $payment = new Payment;
