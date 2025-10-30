94ec8ac4bc0474659b666f3a86e1b96ddf0a50fc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 05:23:54 +0600

    Commit Fri 10/31/2025  5:23:54.23 27495

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,6 +20,7 @@ class Controller extends BaseController
     {
       // etake cache korte hobe sathe sathe reported question gular cound dhukaite hobe
       $unresolvedmessagecount = Message::where('status', 0)->count();
+      dd(Auth::user());
       if(Auth::user()->local_office_id) {
         $localoffice = LocalOffice::findOrFail(Auth::user()->local_office_id);
         $packageexpirycheck = isPackageExpired($localoffice->package_expiry_date);
