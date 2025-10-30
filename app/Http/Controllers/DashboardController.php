2796de5f117d5450b6ae726f4da70c088a03c339
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 01:47:27 +0600

    Commit Fri 10/31/2025  1:47:27.25 29087

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -628,9 +628,9 @@ class DashboardController extends Controller
 
     public function getOfficePayment()
     {
-        $payments = Payment::orderBy('id', 'desc')->paginate(15);
+        $packages = Package::all();
         
-        return view('dashboard.payments.index')->withPayments($payments);
+        return view('dashboard.packages.index')->withPackages($packages);
     }
 
     public function getExamsToday()
