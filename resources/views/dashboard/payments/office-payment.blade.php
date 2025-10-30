a3e4ab3ffb84de914e3ec0426a73e6528eb9dccd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 02:33:20 +0600

    Commit Fri 10/31/2025  2:33:20.31 31679

--- resources/views/dashboard/payments/office-payment.blade.php
+++ resources/views/dashboard/payments/office-payment.blade.php
@@ -250,7 +250,7 @@
       <div class="row">
         <div class="col-md-6">
           <div class="adminlte-card-shadow rounded-lg p-4  border-l-4 border-green-500 bg-white">
-              <div class="flex items-start space-x-3">
+              <div class="">
                   
                   <!-- Icon: AdminLTE uses a prominent icon -->
                   <i class="fas fa-circle-info text-2xl text-green-500 mt-0.5"></i>
