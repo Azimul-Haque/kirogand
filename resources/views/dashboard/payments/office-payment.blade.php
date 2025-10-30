804d399b0dd66e70bbd0479c811f22f40ab5e240
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 01:56:40 +0600

    Commit Fri 10/31/2025  1:56:40.28 23826

--- resources/views/dashboard/payments/office-payment.blade.php
+++ resources/views/dashboard/payments/office-payment.blade.php
@@ -146,7 +146,7 @@
                                   <li> <span class="feature-check">✓</span> একাধিক ইউজার যোগ ও ব্যবহারের সুযোগ</li>
                                   <li> <span class="feature-check">✓</span> মডেল টেস্ট ও সাবজেক্টিভ প্রস্তুতি</li>
                                   {{-- আপনি এখানে আপনার লজিক দিয়ে ফিচারগুলো যোগ/বাদ করতে পারেন --}}
-                                  {{-- উদাহরণ: <li> <span class="feature-check text-danger">✖</span> সীমিত সাপোর্ট</li> --}}
+                                  উদাহরণ: <li> <span class="feature-check text-danger">✖</span> সীমিত সাপোর্ট</li>
                               </ul>
                           </div>
                       </div>
