8d512d7c7e2a527a256c5afb5571c7908dd24007
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 03:56:47 +0600

    Commit Fri 10/31/2025  3:56:47.32 25295

--- resources/views/index/payments/bkashpay.blade.php
+++ resources/views/index/payments/bkashpay.blade.php
@@ -45,6 +45,7 @@
         
         <button class="btn btn-danger" id="bKash_button" onclick="BkashPayment()" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
         </button>
+        <span id="changeTextonLoad">উপরের বাটনে ক্লিক করুন</span>
     </div>
 </div>
 
@@ -64,7 +65,7 @@
             width:300px;
             height:150px;
 
-            
+
         }
 
         .image{
