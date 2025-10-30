01afb9818cac8ad7742816b68bdb5311d82cb11d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 05:41:34 +0600

    Commit Fri 10/31/2025  5:41:34.27 19979

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -74,7 +74,7 @@ class DashboardController extends Controller
         if(Auth::user()->local_office_id) {
           $packageexpirycheck = isPackageExpired(Auth::user()->localOffice->package_expiry_date);
           if($packageexpirycheck) {
-            Session::flash('success', 'আপনার নিবন্ধন সফল হয়েছে। অনুমোদনের জন্য অপেক্ষা করুন। আপনার সাথে যোগাযোগ করা হবে। অথবা এই নম্বরে যোগাযোগ করুন: 01xxxxxxxxx');
+            Session::flash('warning', 'আপনার নিবন্ধন সফল হয়েছে। অনুমোদনের জন্য অপেক্ষা করুন। আপনার সাথে যোগাযোগ করা হবে। অথবা এই নম্বরে যোগাযোগ করুন: 01xxxxxxxxx');
             }
         }
 
