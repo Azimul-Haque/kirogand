835823a85914c26ff9971cac39432ff28c1b315d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 22:46:14 +0600

    Commit Fri 10/31/2025 22:46:14.23 13897

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -98,7 +98,7 @@ class CertificateController extends Controller
     ]);
 
     $newuser = User::create([
-        'certificate_type' => $validatedData['certificate_type'],
+        'local_office_id ' => $validatedData['certificate_type'],
         'recipient_user_id' => Auth::check() ? Auth::id() : null,
         'status' => 0, // 0 = draft, 1 = published
         'unique_serial' => $uniqueSerial,
