ad02d68b9b35efed703f098189162387acb7ca4f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 19:56:09 +0600

    Commit Fri 10/31/2025 19:56:09.30 18467

--- resources/views/dashboard/certificates/index.blade.php
+++ resources/views/dashboard/certificates/index.blade.php
@@ -168,6 +168,15 @@
               <a href="/service/death-certificate" class="text-decoration-none text-dark d-block">
                   <div class="service-box">
                       <div class="icon-circle" style="background-color: #dc3545;"><i class="fas fa-cross"></i></div> 
+                      <h3 class="h5 font-weight-bold" style="color: var(--darker-color);">মৃত্যু সনদ</h3>
+                  </div>
+              </a>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-sm-6 col-6 service-box-container" data-service-id="2">
+              <a href="/service/death-certificate" class="text-decoration-none text-dark d-block">
+                  <div class="service-box">
+                      <div class="icon-circle" style="background-color: #dc3545;"><i class="fas fa-cross"></i></div> 
                       <h3 class="h5 font-weight-bold" style="color: var(--darker-color);">মৃত্যু নিবন্ধন সনদ</h3>
                   </div>
               </a>
