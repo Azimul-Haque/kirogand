41fc8b25d74ba1afb2903266751f2f1bfb4896ea
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 02:23:53 +0600

    Commit Sat 11/01/2025  2:23:53.28 12794

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -157,7 +157,7 @@
                 @php
                     $image_path = public_path('images/localoffices/'. $certificate->recipient->localOffice->monogram);
                     if(File::exists($image_path)) {
-                        
+                        $image_url = $image_path;
                     }
                 @endphp
                 <div class="logo-box" style="float: right;">
