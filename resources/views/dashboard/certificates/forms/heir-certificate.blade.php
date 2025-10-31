1c69d3257300aba4af39a0da42049c7b405ceae2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 22:05:56 +0600

    Commit Fri 10/31/2025 22:05:56.26 9667

--- resources/views/dashboard/certificates/forms/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/forms/heir-certificate.blade.php
@@ -22,7 +22,7 @@
             <div class="row">
                 <!-- নাম -->
                 <div class="form-group col-md-6">
-                    <label for="name">নাম (মৃত বা আবেদনকারী) <span class="text-danger">*</span></label>
+                    <label for="name">নাম (আবেদনকারী) <span class="text-danger">*</span></label>
                     <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required>
                     @error('name') <span class="invalid-feedback">{{ $message }}</span> @enderror
                 </div>
