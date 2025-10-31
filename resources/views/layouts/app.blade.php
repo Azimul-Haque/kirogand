4ff8981b5e54c93c7bb6bae5caf5be07d41e6c26
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 01:14:21 +0600

    Commit Sat 11/01/2025  1:14:21.26 21431

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -167,7 +167,7 @@
 
 <script src="{{ mix('js/app.js') }}" defer></script>
 <script src="{{ asset('js/pace.min.js') }}"></script>
-
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
     Pace.restart();
     $(document).ready(function(){
