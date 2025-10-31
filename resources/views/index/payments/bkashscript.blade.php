630ec0cb42e59c66d9f2551bfbc3be9f9dff3aee
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 06:11:30 +0600

    Commit Fri 10/31/2025  6:11:30.20 30356

--- resources/views/index/payments/bkashscript.blade.php
+++ resources/views/index/payments/bkashscript.blade.php
@@ -143,63 +143,7 @@
 
 
 <script>
-        // Function to convert base64 to ArrayBuffer (required for the environment)
-        const base64ToArrayBuffer = (base64) => {
-            const binaryString = atob(base64);
-            const len = binaryString.length;
-            const bytes = new Uint8Array(len);
-            for (let i = 0; i < len; i++) {
-                bytes[i] = binaryString.charCodeAt(i);
-            }
-            return bytes.buffer;
-        };
-
-        // Function to create WAV blob from PCM data (required for the environment)
-        const pcmToWav = (pcm16, sampleRate) => {
-            const numChannels = 1;
-            const bytesPerSample = 2;
-            const blockAlign = numChannels * bytesPerSample;
-            const byteRate = sampleRate * blockAlign;
-            const dataSize = pcm16.length * bytesPerSample;
-            const buffer = new ArrayBuffer(44 + dataSize);
-            const view = new DataView(buffer);
-
-            let offset = 0;
-
-            const writeString = (str) => {
-                for (let i = 0; i < str.length; i++) {
-                    view.setUint8(offset + i, str.charCodeAt(i));
-                }
-                offset += str.length;
-            };
-
-            // RIFF chunk
-            writeString('RIFF');
-            view.setUint32(4, 36 + dataSize, true);
-            writeString('WAVE');
-
-            // FMT sub-chunk
-            writeString('fmt ');
-            view.setUint32(16, 16, true); // Sub-chunk size: 16
-            view.setUint16(20, 1, true);  // Audio format (1 for PCM)
-            view.setUint16(22, numChannels, true);
-            view.setUint32(24, sampleRate, true);
-            view.setUint32(28, byteRate, true);
-            view.setUint16(32, blockAlign, true);
-            view.setUint16(34, 16, true); // Bits per sample
-
-            // DATA sub-chunk
-            writeString('data');
-            view.setUint32(40, dataSize, true);
-
-            // PCM data
-            const dataView = new DataView(buffer, 44);
-            for (let i = 0; i < pcm16.length; i++) {
-                dataView.setInt16(i * 2, pcm16[i], true);
-            }
-
-            return new Blob([buffer], { type: 'audio/wav' });
-        };
+        
 
 
         document.addEventListener('DOMContentLoaded', () => {
