03caa30dab4036f3d3fd991362beeaf972f5cbd9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 23:14:01 +0600

    Commit Fri 10/31/2025 23:14:01.23 10818

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -143,8 +143,9 @@ class CertificateController extends Controller
 
         $certificate = Certificate::where('local_office_id', Auth::user()->local_office_id)
                                   ->where('unique_serial', $unique_serial)
+                                  ->first();
 
-        return view('dashboard.certificates.create')->with('certificate_type', $certificate_type);
+        return view('dashboard.certificates.draft')->with('certificate_type', $certificate_type);
     }
     
 }
