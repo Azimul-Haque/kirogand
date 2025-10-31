472e9ab449d31561e5e01a738c0aae5cfc64eba7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 00:36:40 +0600

    Commit Sat 11/01/2025  0:36:40.29 25341

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -42,6 +42,15 @@
 </li>
 @endif
 
+@if(Auth::user()->role == 'admin' || Auth::user()->role == 'manager')
+<li class="nav-item">
+    <a href="{{ route('dashboard.certificates.index') }}" class="nav-link {{ Request::is('dashboard/certificates') ? 'active' : '' }} {{ Request::is('dashboard/certificates/*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-certificate"></i>
+        <p>সনদের আবেদনসমূহ</p>
+    </a>
+</li>
+@endif
+
 @if(Auth::user()->role == 'manager')
 <li class="nav-item">
     <a href="{{ route('dashboard.payments.office') }}" class="nav-link {{ Request::is('dashboard/payments/office/payment') ? 'active' : '' }} {{ Request::is('dashboard/payments/office/payment/*') ? 'active' : '' }}">
