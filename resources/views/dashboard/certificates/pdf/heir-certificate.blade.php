de055a34ae83ef3e7c323f9c8c352a6597cf27d9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 02:21:17 +0600

    Commit Sat 11/01/2025  2:21:17.30 28642

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -154,10 +154,12 @@
                 <span style="font-size: 13px;">ফোন নম্বর : {{ $union_info['phone'] }}</span>
             </td>
             <td style="width: 30%; text-align: right;">
-                $image_path = public_path('images/localoffices/'. $localoffice->monogram);
-                if(File::exists($image_path)) {
-                    File::delete($image_path);
-                }
+                @php
+                    $image_path = public_path('images/localoffices/'. $localoffice->monogram);
+                    if(File::exists($image_path)) {
+                        File::delete($image_path);
+                    }
+                @endphp
                 <div class="logo-box" style="float: right;">
                      <img src="{{ public_path('images/eprottyon-logo.png') }}" style="height: 100px; width: auto; object-fit: contain;">
                 </div>
