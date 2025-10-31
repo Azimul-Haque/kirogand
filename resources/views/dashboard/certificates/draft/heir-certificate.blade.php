e8ce97efc46dc2ef6e7bb7b0973796245d1b0ba4
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 00:53:05 +0600

    Commit Sat 11/01/2025  0:53:05.30 18681

--- resources/views/dashboard/certificates/draft/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/draft/heir-certificate.blade.php
@@ -157,7 +157,7 @@
                 <form action="{{ route('dashboard.certificates.update', $certificate->id) }}" method="POST" style="display:inline;">
                     @csrf
                     @method('DELETE')
-                        <button type="submit" class="btn btn-success no-print" onclick="return ToastAprv.fire({ icon: 'success', title: 'আপনি কি নিশ্চিত? এই তথ্য স্থায়ীভাবে অনুমোদন করা হবে।' })">
+                        <button type="button" class="btn btn-success no-print" onclick="return ToastAprv.fire({ icon: 'success', title: 'আপনি কি নিশ্চিত? এই তথ্য স্থায়ীভাবে অনুমোদন করা হবে।' })">
                             <i class="fas fa-print"></i> অনুমোদন করুন
                         </button>
                         <script type="text/javascript">
