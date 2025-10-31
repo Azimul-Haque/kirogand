b9c90778dcdc38865aa130134ff1938b5cb67e08
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 04:35:51 +0600

    Commit Sat 11/01/2025  4:35:51.30 354

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -252,7 +252,7 @@
         {{-- Signature and Seal Block --}}
         <table class="signature-block">
             <tr>
-                <td>
+                <td width="33%">
                     <img src="{{ public_path('images/seal-placeholder.png') }}" style="height: 70px; width: auto; display: block; margin: 0 auto;">
                 </td>
                 <td>
