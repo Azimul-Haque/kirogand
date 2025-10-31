089760b4ad07ff0395d45bfdcb64e298826e80a7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 23:19:34 +0600

    Commit Fri 10/31/2025 23:19:34.31 13471

--- resources/views/dashboard/certificates/draft.blade.php
+++ resources/views/dashboard/certificates/draft.blade.php
@@ -24,7 +24,7 @@
       <div class="row">
         <div class="col-md-10 offset-md-1">
           @if($certificate_type == 'heir-certificate')
-            @include('dashboard.certificates.forms.heir-certificate')
+            @include('dashboard.certificates.draft.heir-certificate')
           @endif
         </div>
       </div>
