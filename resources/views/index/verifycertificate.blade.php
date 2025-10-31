9d6eea92c0612070773a1cb25551e7082b6baf3d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 05:58:39 +0600

    Commit Sat 11/01/2025  5:58:39.32 21706

--- resources/views/index/verifycertificate.blade.php
+++ resources/views/index/verifycertificate.blade.php
@@ -65,7 +65,7 @@
 @section('third_party_scripts')
   <script type="text/javascript">
     $('#verifyButton').click(function(e) {
-      console.log('clicked');
+      
     });
   </script>
 @endsection
