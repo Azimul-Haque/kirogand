6bb828666860f09cea0a83a512a4f3a4f52bc1a6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 05:06:47 +0600

    Commit Sat 11/01/2025  5:06:47.31 2625

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <style>
         body {
-            font-family: 'kalpurush', sans-serif;
+            font-family: 'Nikosh', sans-serif;
             color: #333;
         }
 
