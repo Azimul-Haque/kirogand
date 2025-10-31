9fff2e6e325db13dc369b4f9f597b56d95cf07c0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 21:25:30 +0600

    Commit Fri 10/31/2025 21:25:30.26 30585

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -69,7 +69,7 @@ class CertificateController extends Controller
         // $localofficescount = LocalOffice::count();
         // $localoffices = LocalOffice::where('name_bn', '!=', '')->orderBy('id', 'desc')->paginate(10);
 
-        return view('dashboard.certificates.create')->withCertificate_Type($certificate_type);
+        return view('dashboard.certificates.create')->withCertificate_type($certificate_type);
     }
 
     public function storeCertificate(Request $requests, $certificate_type)
