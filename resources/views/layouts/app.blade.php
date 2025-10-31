33be47729dbfa204c4a501959e612a0a7b93eb8d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 01:14:34 +0600

    Commit Sat 11/01/2025  1:14:34.27 24576

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -170,10 +170,8 @@
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
     Pace.restart();
-    $(document).ready(function(){
-      $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
       $("[rel='tooltip']").tooltip();
-    });
     
     function bangla(str) {
       // English to Bangla digits
