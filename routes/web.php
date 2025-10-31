1f85420b5b5467e00e177aeb3719cd2ad5f2a17f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 00:03:45 +0600

    Commit Sat 11/01/2025  0:03:45.24 16415

--- routes/web.php
+++ routes/web.php
@@ -133,7 +133,7 @@ Route::get('/dashboard/messages/delete/{id}', 'DashboardController@deleteMessage
 Route::get('/dashboard/certificates', 'CertificateController@index')->name('dashboard.certificates.index');
 Route::get('/dashboard/certificates/create/{certificate_type}', 'CertificateController@createCertificate')->name('dashboard.certificates.create');
 Route::get('/dashboard/certificates/draft/{unique_serial}', 'CertificateController@getDraftCertificate')->name('dashboard.certificates.draft');
-Route::get('/dashboard/certificates/edit/{unique_serial}', 'CertificateController@editCertificate')->name('dashboard.certificates.certificate_type');
+Route::get('/dashboard/certificates/edit/{unique_serial}', 'CertificateController@editCertificate')->name('dashboard.certificates.edit');
 Route::post('/dashboard/certificates/store/{certificate_type}', 'CertificateController@storeCertificate')->name('dashboard.certificates.store');
 // Certificate routes are here
 // Certificate routes are here
