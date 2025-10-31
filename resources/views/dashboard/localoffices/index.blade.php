75b05724adcdb6d5a96f4809407c0c85c9784a52
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 15:39:40 +0600

    Commit Fri 10/31/2025 15:39:40.21 29201

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -125,7 +125,7 @@
                                     <div class="input-group mb-3">
                                         <input type="text"
                                                class="form-control"
-                                               placeholder="ইংরেজি নাম (OPTIONAL)" disabled>
+                                               placeholder="ইংরেজি নাম" disabled>
                                         <div class="input-group-append">
                                             <div class="input-group-text"><span class="fas fa-user"></span></div>
                                         </div>
