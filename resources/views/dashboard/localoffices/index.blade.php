7cf7921351d37336dcb7f66ed441bc8442c3fa78
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 15:31:15 +0600

    Commit Fri 10/31/2025 15:31:15.21 1083

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -94,7 +94,7 @@
                             <span aria-hidden="true">&times;</span>
                           </button>
                         </div>
-                        <form method="post" action="{{ route('dashboard.local-offices.payment.add', $localoffice->id) }}" enctype="multipart/form-data">
+                        <form method="post" action="{{ route('dashboard.local-offices.payment.add', $localoffice->id) }}">
                           <div class="modal-body">
                             @php
                               $authlevel = '';
