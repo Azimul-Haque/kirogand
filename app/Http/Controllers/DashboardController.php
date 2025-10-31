b79cfcdb86575786e6f8d6be1ec6abac5a2e17ea
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 02:26:29 +0600

    Commit Sat 11/01/2025  2:26:29.26 11740

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -533,7 +533,7 @@ class DashboardController extends Controller
                 File::delete($image_path);
             }
             $image      = $request->file('monogram');
-            $filename   = strtolower($request->office_type) . '-monogram-' .time() . '.' . "webp";
+            $filename   = strtolower($request->office_type) . '-monogram-' .time() . '.' . "png";
             $location   = public_path('images/localoffices/'. $filename);
             // Image::make($image)->resize(600, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
             Image::make($image)->fit(300, 300)->save($location);
