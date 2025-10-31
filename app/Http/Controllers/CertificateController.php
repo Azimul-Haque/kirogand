c4bdfe80edb75f097661154ccba772e15d59b4ff
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 22:48:07 +0600

    Commit Fri 10/31/2025 22:48:07.24 22549

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -104,7 +104,7 @@ class CertificateController extends Controller
         'name ' => $request->name,
         'role ' => 'user', // this is important
         'designation ' => 'নাগরিক',
-        'designation ' => 'নাগরিক',
+        'mobile' => 'নাগরিক',
 
         'recipient_user_id' => Auth::check() ? Auth::id() : null,
         'status' => 0, // 0 = draft, 1 = published
