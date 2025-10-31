55e31076f3a63e002dc492eb5ba458425bbda232
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 01:07:18 +0600

    Commit Sat 11/01/2025  1:07:18.24 28159

--- resources/views/dashboard/certificates/list.blade.php
+++ resources/views/dashboard/certificates/list.blade.php
@@ -96,7 +96,7 @@
                                 <form action="{{ route('dashboard.certificates.approve', $certificate->id) }}" id="approveForm{{ $certificate->id }}" method="POST" style="display:inline;">
                                     @csrf
                                     <button type="submit" class="btn btn-success no-print" onclick="return confirmSubmission(event);">
-                                        <i class="fas fa-print"></i> অনুমোদন করুন
+                                        <i class="fas fa-check"></i> অনুমোদন
                                     </button>
                                 </form>
                                 @endif
