1b474a150b1c1208c0a0a2bb119e30670c0d4bba
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 15:44:59 +0600

    Commit Fri 10/31/2025 15:44:59.26 18181

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -58,7 +58,7 @@
                 @foreach($localoffices as $localoffice)
                   <tr>
                     <td>
-                      {{ $localoffice->name_bn }} <small>({{ bangla($localoffice->payments->count()) বার }})</small></br>
+                      {{ $localoffice->name_bn }} <small>({{ bangla($localoffice->payments->count()) }} বার)</small></br>
                       <span class="badge badge-primary">📞 {{ $localoffice->mobile }}</span>
                       <span class="badge badge-warning">✉ {{ $localoffice->email }}</span>
                     </td>
