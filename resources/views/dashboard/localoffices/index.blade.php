126a39814d0e7c6e07594e39effb0d640719223d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 15:27:56 +0600

    Commit Fri 10/31/2025 15:27:56.29 5202

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -77,7 +77,7 @@
                       <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editLocalOfficeModal{{ $localoffice->id }}">
                         <i class="fas fa-pen"></i>
                       </button>
-                      <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#editLocalOfficeModal{{ $localoffice->id }}">
+                      <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#addPaymentModal{{ $localoffice->id }}">
                         <i class="fas fa-pen"></i>
                       </button>
                     </td>
