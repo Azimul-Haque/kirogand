d89ed07380ee575e3c0c08820052d47b12203360
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 05:32:44 +0600

    Commit Sat 11/01/2025  5:32:44.29 31166

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -242,7 +242,7 @@ class CertificateController extends Controller
         $certificate = Certificate::where('unique_serial', $unique_serial)->first();
 
         $dataToEncode = url("/verify/{$certificate->unique_serial}");
-        $qrCodeSvg = QrCode::format('png')->size(200)->generate($dataToEncode);
+        $qrCodeSvg = QrCode::size(200)->generate($dataToEncode);
 
         $pdf = PDF::loadView('dashboard.certificates.pdf.heir-certificate', ['certificate' => $certificate, 'qrCodeSvg' => $qrCodeSvg]);
         $fileName = 'Cert-' . $certificate->unique_serial . '.pdf';
