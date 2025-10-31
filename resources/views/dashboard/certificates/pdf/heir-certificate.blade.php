235dd4dfcba49ed3246d80f31681a44a10a7ef07
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 04:04:29 +0600

    Commit Sat 11/01/2025  4:04:29.24 16211

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -59,7 +59,7 @@
             background-color: #e5ffe5; /* Light green background */
             border: 1px solid #c2e6c2;
             padding: 10px 10px;
-            margin: 20px auto;
+            margin: 10px auto;
             width: 40%;
             border-radius: 20px;
             font-size: 18px;
