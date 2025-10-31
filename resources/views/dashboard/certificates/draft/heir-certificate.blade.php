ad82a47ed6883a43a46e0751729f880103aa9419
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 01:08:43 +0600

    Commit Sat 11/01/2025  1:08:43.25 30079

--- resources/views/dashboard/certificates/draft/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/draft/heir-certificate.blade.php
@@ -157,7 +157,7 @@
                 <form action="{{ route('dashboard.certificates.approve', $certificate->id) }}" id="approveForm{{ $certificate->id }}" method="POST" style="display:inline;">
                     @csrf
                     <button type="button" class="btn btn-success no-print" onclick="return confirmSubmission(event);">
-                        <i class="fas fa-print"></i> অনুমোদন করুন
+                        <i class="fas fa-check"></i> অনুমোদন করুন
                     </button>
                 </form>
                 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
