8a6a8dd999816bdc9db0cb93fe9a438e3baa4226
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 01:52:51 +0600

    Commit Sat 11/01/2025  1:52:51.23 23103

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -237,7 +237,8 @@ class CertificateController extends Controller
             }
         }
 
-        $certificate = Certificate::where('unique_serial', $unique_serial)->first()
+        $certificate = Certificate::where('unique_serial', $unique_serial)->first();
+        
         $pdf = PDF::loadView('index.pdf.examsolvepdf', ['exam' => $exam]);
         $fileName = 'Single-Exam-Solve-Sheet-' . $exam->id . '.pdf';
         return $pdf->stream($fileName); // download/stream
