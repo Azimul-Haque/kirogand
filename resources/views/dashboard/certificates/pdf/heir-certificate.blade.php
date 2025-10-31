f5dbfac48917ac67c0d5b76f7a041701f98d57e8
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 02:16:20 +0600

    Commit Sat 11/01/2025  2:16:20.21 4504

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -16,7 +16,7 @@
             background-repeat: no-repeat;
             background-position: center center;
             /* Reduced top/bottom margin for better content display */
-            margin: 60px 60px 60px 60px;
+            margin: 100px 100px 100px 100px;
         }
 
         /* --- Custom Styles for Certificate Layout --- */
