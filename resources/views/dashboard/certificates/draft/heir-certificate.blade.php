a9bb1ed048471b9f1953f1655c73c9de29ebaa79
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 00:54:47 +0600

    Commit Sat 11/01/2025  0:54:47.31 26431

--- resources/views/dashboard/certificates/draft/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/draft/heir-certificate.blade.php
@@ -161,7 +161,7 @@
                     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
                     <script type="text/javascript">
                       const ToastAprv = Swal.mixin({
-                        toast: false,
+                        toast: true,
                         position: 'center',
                         showConfirmButton: true,
                         showCancelButton: true,
