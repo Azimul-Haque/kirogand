cdcee11f2515e11b78ac829231880837169c5a31
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 00:15:41 +0600

    Commit Sat 11/01/2025  0:15:41.30 27203

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -171,7 +171,7 @@ class CertificateController extends Controller
             }
         }
 
-        $certificates = Certificate::where('local_office_id', Auth::user()->local_office_id) // this is important
+        $certificates = Certificate::where('local_office_id', Auth::user()->local_office_id)->first(); // this is important
 
         return view('dashboard.certificates.list')->withCertificates($certificates);
     }
