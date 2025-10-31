8a75113aac3efb53ec7b905b6a223080fd9441ac
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 21:31:51 +0600

    Commit Fri 10/31/2025 21:31:51.22 8298

--- resources/views/dashboard/certificates/create.blade.php
+++ resources/views/dashboard/certificates/create.blade.php
@@ -66,6 +66,7 @@
                         <input type="hidden" name="certificate_type" value="{{ old('certificate_type', $selectedType ?? '') }}">
                         
                         <!-- Selector sends a GET request to reload the form with the new schema -->
+                        <input name="certificate_type_selector">
                         <select name="certificate_type_selector" id="certificate_type_selector" 
                                 onchange="window.location.href = '{{ route('dashboard.certificates.certificate_type', $certificate_type) }}?certificate_type=' + this.value"
                                 class="form-input-field text-lg appearance-none bg-white cursor-pointer">
