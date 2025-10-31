3586872f1909725e7af22ffc7584cc0d33ad28fc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-10-31 15:39:19 +0600

    Commit Fri 10/31/2025 15:39:19.27 14456

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -163,38 +163,6 @@
                                     </div>
                                   </div>
                                 </div>
-
-                                
-
-                                <div class="row">
-                                  <div class="col-md-6">
-                                    <div class="form-group">
-                                        
-                                        <!-- Bootstrap 4 Custom File Input -->
-                                        <div class="custom-file">
-                                            <!-- 
-                                                The 'accept' attribute restricts the file picker 
-                                                to show only common image types.
-                                            -->
-                                            <input 
-                                                type="file" 
-                                                class="custom-file-input" 
-                                                id="monogram{{ $localoffice->id }}" 
-                                                name="monogram"
-                                                
-                                                accept="image/png, image/jpeg, image/gif"
-                                                @if($localoffice->monogram == null) required @endif
-                                            >
-                                            <label class="custom-file-label" for="monogram">মনোগ্রাম সিলেক্ট করুন</label>
-                                        </div>
-                                        <small class="form-text text-muted">Max file size 300KB. Accepts PNG, JPG, GIF. (300px X 300px)</small>
-                                    </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                    
-                                  </div>
-                                </div>
-                                
                           </div>
                           <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
