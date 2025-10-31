197f302c21257285965afedb9ce8387eae2b509b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 01:32:59 +0600

    Commit Sat 11/01/2025  1:32:59.25 8738

--- resources/views/dashboard/certificates/list.blade.php
+++ resources/views/dashboard/certificates/list.blade.php
@@ -44,6 +44,9 @@
     <ol class="breadcrumb float-sm-right">
         <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
         <li class="breadcrumb-item active">সনদ তালিকা</li>
+        @if(isset($justapproved))
+          pulsePrintButton({{$justapproved->id}});
+        @endif
     </ol>
   @endsection
     <div class="container-fluid">
