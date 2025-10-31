cff3379d92878be978c6b76200bc953958389237
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 05:12:45 +0600

    Commit Sat 11/01/2025  5:12:45.23 32385

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -79,7 +79,7 @@ class IndexController extends Controller
     public function verifyCertificate($unique_serial)
     {
         $certificate = Certificate::where('unique_serial', $unique_serial)->first();
-        return view('index.verify');
+        return view('index.verify')->withCertificate($certificate);
     }
 
     public function getApplicationStatus()
