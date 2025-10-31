276f14676101c856d47edcd3064f964a047d7ffb
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 00:13:37 +0600

    Commit Sat 11/01/2025  0:13:37.32 22988

---
+++ resources/views/dashboard/certificates/list.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.app')
+@section('title') ড্যাশবোর্ড | পেমেন্ট @endsection
+
+@section('third_party_stylesheets')
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
+
+    <style>
+    
+
+    </style>
+@endsection
+
+@section('content')
+  @section('page-header') পেমেন্ট @endsection
+  @section('page-header-right')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">ড্যাশবোর্ড</a></li>
+        <li class="breadcrumb-item active">পেমেন্ট</li>
+    </ol>
+  @endsection
+    <div class="container-fluid">
+    <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">পেমেন্ট তালিকা</h3>
+
+            <div class="card-tools">
+              {{-- <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addPackageModal" title="" rel="tooltip" data-original-title="পেমেন্ট যোগ করুন">
+                <i class="fas fa-clipboard-check"></i> নতুন পেমেন্ট
+              </button> --}}
+              <div class="card-tools">
+                <form class="form-inline form-group-lg" action="">
+                  <div class="form-group">
+                    <input type="search-param" class="form-control form-control-sm" placeholder="পেমেন্ট খুঁজুন" id="search-param" required>
+                  </div>
+                  <button type="button" id="search-button" class="btn btn-default btn-sm" style="margin-left: 5px;">
+                    <i class="fas fa-search"></i> খুঁজুন
+                  </button>
+                </form>
+                
+              </div>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body p-0">
+            <table class="table">
+              <thead>
+                <thead>
+                  <th>প্রতিষ্ঠান</th>
+                  <th>প্যাকেজ</th>
+                  <th>পেমেন্ট স্ট্যাটাস</th>
+                  <th>পরিশোধের মাধ্যম</th>
+                  <th>ট্রানজেকশন আইডি</th>
+                  <th>পরিমাণ</th>
+                  <th>সময়</th>
+                </thead>
+              </thead>
+              <tbody>
+                @foreach($payments as $payment)
+                  <tr>
+                    <td>
+                      <a href="#!">{{ $payment->localOffice->name_bn }}</a>
+                      <small>({{ $payment->localOffice->payments->count() }} বার)</small><br/>
+                      <small class="text-black-50">{{ $payment->localOffice->mobile }}</small>
+                    </td>
+                    <td>{{ $payment->package->name }}</td>
+                    <td>{{ $payment->payment_status == 1 ? 'Successfull' : 'Failed' }}</td>
+                    <td><span class="badge @if($payment->card_type == 'bKash') badge-success @else badge-primary @endif">{{ $payment->card_type }}</span></td>
+                    <td>{{ $payment->trx_id }}</td>
+                    <td><b>৳ {{ $payment->store_amount }}</b> <small>(৳ {{ $payment->amount }})</small></td>
+                    <td>{{ date('F d, Y h:i A', strtotime($payment->created_at)) }}</td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        {{ $payments->links() }}
+
+    </div>
+@endsection
+
+@section('third_party_scripts')
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="module">
+
+      $(document).on('click', '#search-button', function() {
+        if($('#search-param').val() != '') {
+          var urltocall = '{{ route('dashboard.payments') }}' +  '/' + $('#search-param').val();
+          location.href= urltocall;
+        } else {
+          $('#search-param').css({ "border": '#FF0000 2px solid'});
+          Toast.fire({
+              icon: 'warning',
+              title: 'কিছু লিখে খুঁজুন!'
+          })
+        }
+      });
+      $("#search-param").keyup(function(e) {
+        if(e.which == 13) {
+          if($('#search-param').val() != '') {
+            var urltocall = '{{ route('dashboard.payments') }}' +  '/' + $('#search-param').val();
+            location.href= urltocall;
+          } else {
+            $('#search-param').css({ "border": '#FF0000 2px solid'});
+            Toast.fire({
+                icon: 'warning',
+                title: 'কিছু লিখে খুঁজুন!'
+            })
+          }
+        }
+      });
+    </script>
+@endsection
