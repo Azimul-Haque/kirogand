4bfb2ed098657d8bf0ab523b30b7c6fc611ae71f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 03:32:51 +0600

    Commit Sat 11/01/2025  3:32:51.28 27426

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -132,7 +132,7 @@
             'union_name' => $certificate->localOffice->name_bn ?? 'তথ্য নেই',
             'upazila' => $lglevels['Upazila'] ?? 'তথ্য নেই',
             'district' => $lglevels['District'] ?? 'তথ্য নেই',
-            'chairman_name' => $certificate->localOffice->name_bn,
+            // 'chairman_name' => $certificate->localOffice->name_bn,
             'email' => $certificate->localOffice->email ?? '',
             'phone' => $certificate->localOffice->mobile ?? '',
         ];
