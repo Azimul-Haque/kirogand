7383958f1dfa0d33a0f3a174846dc1e29ef5b336
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 06:21:46 +0600

    Commit Sat 11/01/2025  6:21:46.29 8402

--- resources/views/index/verify.blade.php
+++ resources/views/index/verify.blade.php
@@ -67,7 +67,7 @@
               </div>
           </div>
           @if(isset($certificate))
-            <div class="col-12 col-md-8 col-lg-8 mt-5">
+            <div class="col-12 col-md-8 col-lg-8 p-3">
                             
                 <!-- Card to display the verified data -->
                 <div id="dataCard" class="card shadow-lg border-0 rounded-xl">
