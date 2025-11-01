7eafd7b4e5694e75d71043fc82d3c8543dbbe930
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 06:05:30 +0600

    Commit Sat 11/01/2025  6:05:30.32 29466

--- resources/views/index/verify.blade.php
+++ resources/views/index/verify.blade.php
@@ -31,7 +31,7 @@
                       <div class="mb-5 text-center">
                           <span for="verificationInput" class="fw-bold h4">
                             @if(isset($certificate))
-
+                              সনদটি 
                             @else
 
                             @endif
