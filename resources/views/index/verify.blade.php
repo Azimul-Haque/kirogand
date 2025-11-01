5a1cd5df94a43b2456084352df949423c62f2d53
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-01 06:05:43 +0600

    Commit Sat 11/01/2025  6:05:43.24 14354

--- resources/views/index/verify.blade.php
+++ resources/views/index/verify.blade.php
@@ -31,7 +31,7 @@
                       <div class="mb-5 text-center">
                           <span for="verificationInput" class="fw-bold h4">
                             @if(isset($certificate))
-                              সনদটি 
+                              সনদটি বৈধ!
                             @else
 
                             @endif
