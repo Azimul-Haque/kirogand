c955d632bcad7e5236e375b46ab2d670165fd263
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-03 04:48:31 +0600

    Commit Mon 11/03/2025  4:48:31.32 22151

--- resources/views/partials/_manager_dashboard.blade.php
+++ resources/views/partials/_manager_dashboard.blade.php
@@ -181,7 +181,7 @@
                                 <i class="fas {{ get_certificate_icon_data_en($cert->certificate_type)['icon_class'] }} fa-2x {{ get_certificate_icon_data_en($cert->certificate_type)['color_class'] }}"></i>
                             </div>
                             <div class="product-info">
-                                <a href="{{ route('dashboard.certificates.draft', $cert->unique_serial) }}" class="product-title font-weight-bold">{{ checkcertificatetype($cert->certificate_type) }} - আইডি: #{{ bangla($cert->unique_serial) }} <i class="fas @if($cert->status == 1) fa-check-circle @else fa-hour-glass @endif"></i></a>
+                                <a href="{{ route('dashboard.certificates.draft', $cert->unique_serial) }}" class="product-title font-weight-bold">{{ checkcertificatetype($cert->certificate_type) }} - আইডি: #{{ bangla($cert->unique_serial) }} <i class="fas @if($cert->status == 1) fa-check-circle @else fa-pending @endif"></i></a>
                                 <span class="product-description">
                                     ইস্যু কর্তৃপক্ষ: ({{ $cert->localOffice->name_bn }}), সময়: {{ date('H:i A', strtotime($cert->issued_at)) }}
                                 </span>
