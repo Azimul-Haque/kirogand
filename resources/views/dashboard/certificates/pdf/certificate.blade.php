0715530bf59a61d3007e6270f4aaf05d80d380e6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-03 00:49:12 +0600

    Commit Mon 11/03/2025  0:49:12.21 20068

--- resources/views/dashboard/certificates/pdf/certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/certificate.blade.php
@@ -235,7 +235,7 @@
         background-repeat: no-repeat;
         background-position: center center;
         /* Reduced top/bottom margin for better content display */
-        margin: 180px 80px 80px 80px;
+        margin: 180px 80px 70px 80px;
     }
     .page-header,
       .page-header-space {
