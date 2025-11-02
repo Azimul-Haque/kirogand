5b73e3f245c0ebff6f62d681869d7f5f8d08a110
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 21:31:27 +0600

    Commit Sun 11/02/2025 21:31:27.22 21955

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -127,6 +127,7 @@ class CertificateController extends Controller
             'recipient_user_id' => $newuser->id,
             'status' => 0, // 0 = draft, 1 = published
             'unique_serial' => $uniqueSerial,
+            'memo' => $uniqueSerial,
             'issued_at' => now(),
             'data_payload' => $dataPayload,
         ]);
