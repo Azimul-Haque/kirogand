6307084f9ee5758e4b1e2f7779d1dc1e43319b87
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 22:40:56 +0600

    Commit Sun 11/02/2025 22:40:56.24 29312

--- config/pdf.php
+++ config/pdf.php
@@ -11,46 +11,16 @@ return [
   'tempDir'               => base_path('../temp/'),
   'font_path' => base_path('vendor\mpdf\mpdf\ttfonts'),
   'font_data' => [
-    'kalpurush' => [
-      'R'  => 'kalpurush.ttf',    // regular font
-      'B'  => 'kalpurush.ttf',       // optional: bold font
-      'I'  => 'kalpurush.ttf',     // optional: italic font
-      'BI' => 'kalpurush.ttf', // optional: bold-italic font
-      'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
-      'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
-    ],
+    
     'nikosh' => [
-      'R'  => 'Nikosh.ttf',    // regular font
-      'B'  => 'Nikosh.ttf',       // optional: bold font
-      'I'  => 'Nikosh.ttf',     // optional: italic font
-      'BI' => 'Nikosh.ttf', // optional: bold-italic font
+      'R'  => 'nikosh.ttf',    // regular font
+      'B'  => 'nikosh.ttf',       // optional: bold font
+      'I'  => 'nikosh.ttf',     // optional: italic font
+      'BI' => 'nikosh.ttf', // optional: bold-italic font
       // 'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
       //'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
     ],
-    'Shonar' => [
-      'R'  => 'Shonar.ttf',    // regular font
-      'B'  => 'Shonarb.ttf',       // optional: bold font
-      'I'  => 'Shonar.ttf',     // optional: italic font
-      'BI' => 'Shonar.ttf', // optional: bold-italic font
-      'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
-      //'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
-    ],
-    'SolaimanLipi' => [
-      'R'  => 'SolaimanLipi_20-04-07.ttf',    // regular font
-      'B'  => 'SolaimanLipi_20-04-07b.ttf',       // optional: bold font
-      'I'  => 'SolaimanLipi_20-04-07.ttf',     // optional: italic font
-      'BI' => 'SolaimanLipi_20-04-07.ttf', // optional: bold-italic font
-      'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
-      //'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
-    ],
-    'HindSiliguri' => [
-      'R'  => 'HindSiliguri-Regular.ttf',    // regular font
-      'B'  => 'HindSiliguri-Regular.ttf',       // optional: bold font
-      'I'  => 'HindSiliguri-Regular.ttf',     // optional: italic font
-      'BI' => 'HindSiliguri-Regular.ttf', // optional: bold-italic font
-      'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
-      //'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
-    ],
+    
     // ...add as many as you want.
   ]
 ];
