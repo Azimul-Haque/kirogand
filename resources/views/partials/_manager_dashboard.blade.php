c46a50cf4639fdc9014ff4993ea9c6fedb970d6a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-03 04:28:56 +0600

    Commit Mon 11/03/2025  4:28:56.29 21990

--- resources/views/partials/_manager_dashboard.blade.php
+++ resources/views/partials/_manager_dashboard.blade.php
@@ -178,7 +178,7 @@
                     @foreach($last5certs as $cert)
                         <li class="item">
                             <div class="product-img">
-                                <i class="fas fa-user-check fa-2x text-success"></i>
+                                <i class="fas fa-user-check fa-2x {{ get_certificate_icon_data_en($cert->certificate_type)['color_classr'] }}"></i>
                             </div>
                             <div class="product-info">
                                 <a href="javascript:void(0)" class="product-title font-weight-bold">নাগরিক সনদ - আইডি: #৯৮৭৬</a>
