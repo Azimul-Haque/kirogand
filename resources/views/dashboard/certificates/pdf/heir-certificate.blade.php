33c37117c721514ad7fd9f78e87a4b18bd8ae9a5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 23:01:02 +0600

    Commit Sun 11/02/2025 23:01:02.30 26609

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -202,8 +202,6 @@
         font-family: 'kalpurush', sans-serif;
         color: #333;
         border: 2px solid black; /* Adjust thickness and color as needed */
-        padding: 10mm; /* Add padding to prevent content from touching the border */
-        margin: 0; /* Remove default body margin */
     }
     @php
         $back_image = public_path('images/localoffices/background-'. $certificate->localOffice->monogram);
