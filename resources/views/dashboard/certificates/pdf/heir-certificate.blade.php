908caa83c8440dd8e139079abea15c30b22eb69d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 22:53:51 +0600

    Commit Sun 11/02/2025 22:53:51.26 14962

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -297,7 +297,7 @@
                     <td style="width: 85%; padding-left: 20px;">
                         <span style=" font-size: 13px;">
                             সনদটি যাচাই করতে <span style="font-family: Calibri;">QR CODE</span> টি স্ক্যান করুন<br/>
-                            অথবা <span style="font-family: Calibri;">www.dnagorik.com</span> ভিজিট করুন এই আইডিটি যাচাই করুন: <span style="font-family: Calibri;">{{ $certificate->unique_serial ?? '--' }}</span>
+                            অথবা <span style="font-family: Calibri;">www.dnagorik.com</span> ভিজিট করে এই আইডিটি যাচাই করুন: <span style="font-family: Calibri;">{{ $certificate->unique_serial ?? '--' }}</span>
                         </span>
                         <p style="font-size: 10px; margin-top: 10px; line-height: 1.5;">
                             বি:দ্র: তথ্য গোপন বা ভুল দিলে আবেদনকারী দায়ী থাকবেন, কর্তৃপক্ষ দায়ী নয়, সনদ বাতিলযোগ্য।
