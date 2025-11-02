eb98c57461b1b5dc4fcd0c2f7ffa532f21d4644f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 21:42:40 +0600

    Commit Sun 11/02/2025 21:42:40.27 1056

--- resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/heir-certificate.blade.php
@@ -255,7 +255,7 @@
                 <td>
                     <img src="{{ public_path('images/seal-placeholder.png') }}" style="height: 70px; width: auto; display: block; margin: 0 auto;">
                 </td>
-                @if($certificate->signatory == 2)
+                @if($certificate->localOffice->signatory == 2)
                     <td>
                         <div style="padding-top: 10px;">প্রস্তুতকারীর সিল ও স্বাক্ষর</div>
                         <div class="signature-line"></div>
