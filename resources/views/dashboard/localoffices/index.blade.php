f338cea6396e492bcf84a21bca0c4e64968c308a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 20:27:43 +0600

    Commit Sun 11/02/2025 20:27:43.25 8324

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -281,6 +281,18 @@
                                         </div>
                                     </div>
                                   </div>
+                                  <div class="col-md-6">
+                                    <div class="input-group mb-3">
+                                      <select name="office_type" class="form-control" required>
+                                        <option disabled="" value="">ধরন নির্ধারণ করুন</option>
+                                        <option value="up" @if($localoffice->office_type == 'up') selected="" @endif>ইউনিয়ন পরিষদ</option>
+                                        <option value="poura" @if($localoffice->office_type == 'poura') selected="" @endif>পৌরসভা</option>
+                                      </select>
+                                        <div class="input-group-append">
+                                            <div class="input-group-text"><span class="fas fa-landmark"></span></div>
+                                        </div>
+                                    </div>
+                                  </div>
                                 </div>
 
                                 <div class="row">
