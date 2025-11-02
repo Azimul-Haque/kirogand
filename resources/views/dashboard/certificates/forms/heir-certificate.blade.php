855913c42116d994ffa3112329b76223821ca116
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 21:24:30 +0600

    Commit Sun 11/02/2025 21:24:30.29 17426

--- resources/views/dashboard/certificates/forms/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/forms/heir-certificate.blade.php
@@ -43,13 +43,6 @@
                            value="{{ $memoValue }}" placeholder="স্মারক নাম্বার" required>
                     @error('memo') <span class="invalid-feedback">{{ $message }}</span> @enderror
                 </div>
-                <div class="form-group col-md-6">
-                    <select name="signatory" class="form-control" required>
-                      <option disabled="" value="">সনদে স্বাক্ষর সংখ্যা</option>
-                      <option value="1" @if($localoffice->signatory == 1) selected="" @endif>শুধু অনুমোদনকারী (চেয়ারম্যান/মেয়র ইত্যাদি)</option>
-                      <option value="2" @if($localoffice->signatory == 2) selected="" @endif>প্রস্তুতকারী ও অনুমোদনকারী</option>
-                    </select>
-                </div>
             </div>
 
             <div class="row">
