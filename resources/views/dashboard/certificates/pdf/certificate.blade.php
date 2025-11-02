6e4a1354d3fd402c69584d48fbea13293a4090e1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-03 00:12:04 +0600

    Commit Mon 11/03/2025  0:12:04.24 329

--- resources/views/dashboard/certificates/pdf/certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/certificate.blade.php
@@ -232,7 +232,7 @@
     .draft-watermark {
         position: absolute;
         top: 50%;
-        left: 50%;
+        left: 30%;
         transform: translate(-50%, -50%) rotate(-45deg);
         color: rgba(255, 0, 0, 0.2);
         font-size: 100px;
