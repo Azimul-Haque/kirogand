22211194a44b293e0a0c2cfd44b294c608b32120
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-03 00:33:53 +0600

    Commit Mon 11/03/2025  0:33:53.28 18433

--- resources/views/dashboard/certificates/pdf/certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/certificate.blade.php
@@ -161,7 +161,7 @@
                         <div class="signature-line"></div>
                         <div style="border-top: 2px solid green; margin-top: 5px;"></div>
                     </td>
-                    <td>
+                    <td align="right">
                         <div style="padding-top: 10px;">অনুমোদনকারীর সিল ও স্বাক্ষর</div>
                         <div class="signature-line"></div>
                     </td>
