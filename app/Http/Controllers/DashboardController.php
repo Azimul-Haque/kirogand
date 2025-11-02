ef9ec05c7a95a6ca5c75f33c62f9ce465f686c30
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 20:49:50 +0600

    Commit Sun 11/02/2025 20:49:50.25 8090

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -519,8 +519,8 @@ class DashboardController extends Controller
         $localoffice->mobile            = $request->mobile;
         $localoffice->email             = $request->email;
         $localoffice->office_type       = $request->office_type;
-        $localoffice->draft_memo       = $request->draft_memo;
-        $localoffice->signatory       = $request->signatory;
+        $localoffice->draft_memo        = $request->draft_memo;
+        $localoffice->signatory         = $request->signatory;
         // if($request->packageexpirydate) {
         //     $localoffice->package_expiry_date = date('Y-m-d', strtotime($request->packageexpirydate)) . ' 23:59:59';
         // } else {
