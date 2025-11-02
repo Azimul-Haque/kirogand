c9815a7acc139d11c8af19cf9a5c0feaf10e3268
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 20:26:09 +0600

    Commit Sun 11/02/2025 20:26:09.24 14577

--- resources/views/dashboard/localoffices/index.blade.php
+++ resources/views/dashboard/localoffices/index.blade.php
@@ -270,6 +270,9 @@
 
                                 <div class="row">
                                   <div class="col-md-6">
+                                </div>
+                                <div class="row">
+                                  <div class="col-md-6">
                                     <div class="input-group mb-3">
                                       <select name="office_type" class="form-control" required>
                                         <option disabled="" value="">ধরন নির্ধারণ করুন</option>
