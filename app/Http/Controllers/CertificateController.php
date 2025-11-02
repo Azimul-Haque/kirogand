d76bdc1aa80262117744df4bc248656e7962c1a2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-02 23:37:16 +0600

    Commit Sun 11/02/2025 23:37:16.29 8863

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -95,7 +95,7 @@ class CertificateController extends Controller
                 'heirs_data.*.dob' => ['nullable', 'string'],
                 'heirs_data.*.remark' => ['nullable', 'string', 'max:255'],
             ]); 
-        } elseif() {
+        } elseif($certificate_type == 'citizen-certificate') {
             $validatedData = $request->validate([
                 'name' => ['required', 'string', 'max:255'],
                 'father' => ['required', 'string', 'max:255'],
