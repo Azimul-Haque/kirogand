b1d22fc366dbbc3f5eef3c29684d51e32c19fb28
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 01:54:13 +0600

    Commit Tue 11/04/2025  1:54:13.30 18746

--- resources/views/dashboard/certificates/index.blade.php
+++ resources/views/dashboard/certificates/index.blade.php
@@ -178,7 +178,7 @@
         <div class="col-lg-2 col-md-4 col-sm-6 col-6 service-box-container" data-service-id="5">
             <a href="{{ route('dashboard.certificates.create', 'landless-certificate') }}" class="text-decoration-none text-dark d-block">
                 <div class="service-box">
-                    <div class="icon-circle" style="background-color: var(--medium-color);"><i class="fas fa-id-card"></i></div> 
+                    <div class="icon-circle" style="background-color: var(--medium-color);"><i class="fas fa-money-bill-alt"></i></div> 
                     <h3 class="h5 font-weight-bold" style="color: var(--darker-color);">মাসিক আয়ের প্রত্যয়ন</h3>
                 </div>
             </a>
