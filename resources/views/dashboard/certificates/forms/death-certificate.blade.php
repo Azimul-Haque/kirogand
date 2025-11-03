0995b9e41635b1091a823ac22a15401c4c557648
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 00:57:04 +0600

    Commit Tue 11/04/2025  0:57:04.22 13274

--- resources/views/dashboard/certificates/forms/death-certificate.blade.php
+++ resources/views/dashboard/certificates/forms/death-certificate.blade.php
@@ -65,7 +65,7 @@
                     <input type="date" class="form-control @error('death_date') is-invalid @enderror" id="death_date" name="death_date"
                            value="{{ old('death_date', $applicant['death_date'] ?? date('Y-m-d')) }}" max="{{ date('Y-m-d') }}" placeholder="মৃত্যুর তারিখ" data-date="" data-date-format="DD MMMM YYYY" required>
                     @error('death_date') <span class="invalid-feedback">{{ $message }}</span> @enderror
-                    <script type="text/javascript" src="{{ asset('js/jquery-for-dp.min.js') }}"></script>
+                    {{-- <script type="text/javascript" src="{{ asset('js/jquery-for-dp.min.js') }}"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js"></script>
                     <script>
                         $("#death_date").on("change", function() {
@@ -75,7 +75,7 @@
                                 .format( this.getAttribute("data-date-format") )
                             )
                         }).trigger("change")
-                    </script>
+                    </script> --}}
                 </div>
                 <!-- জাতীয় পরিচয়পত্র / জন্ম নিবন্ধন -->
                 <div class="form-group col-md-6">
