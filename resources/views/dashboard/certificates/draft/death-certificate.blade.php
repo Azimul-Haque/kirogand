b9ff79baa8754e041b6659c50f68c0dc7a73fdee
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 00:59:48 +0600

    Commit Tue 11/04/2025  0:59:48.31 26942

--- resources/views/dashboard/certificates/draft/death-certificate.blade.php
+++ resources/views/dashboard/certificates/draft/death-certificate.blade.php
@@ -64,6 +64,10 @@
                                 <div class="col-md-6">
                                     <p><span class="info-label">স্মারক:</span> {{ $certificate->memo ?? 'N/A' }}</p>
                                 </div>
+                                <div class="col-md-6">
+                                    <p><span class="info-label">স্মারক:</span> {{ $certificate->memo ?? 'N/A' }}</p>
+                                    <p><span class="info-label">স্মারক:</span> {{ $certificate->memo ?? 'N/A' }}</p>
+                                </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-6">
