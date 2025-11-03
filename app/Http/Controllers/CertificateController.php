9f5eb6f37abf5b3a9292429a86ad5559acf72204
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 02:18:40 +0600

    Commit Tue 11/04/2025  2:18:40.30 7967

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -130,6 +130,7 @@ class CertificateController extends Controller
             $validatedData = $request->validate([
                 'name' => ['required', 'string', 'max:255'],
                 'earner' => ['required', 'string', 'max:255'],
+                'profession' => ['required', 'string', 'max:255'],
                 'income' => ['required', 'string', 'max:255'],
                 'father' => ['required', 'string', 'max:255'],
                 'mother' => ['required', 'string', 'max:255'],
