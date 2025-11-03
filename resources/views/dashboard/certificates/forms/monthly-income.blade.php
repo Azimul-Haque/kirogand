733ae6d684359ab77f8b34b746bc35250a8ea615
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 02:00:18 +0600

    Commit Tue 11/04/2025  2:00:18.21 28761

--- resources/views/dashboard/certificates/forms/monthly-income.blade.php
+++ resources/views/dashboard/certificates/forms/monthly-income.blade.php
@@ -45,7 +45,7 @@
                     @error('memo') <span class="invalid-feedback">{{ $message }}</span> @enderror
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="death_reg_no">মৃত্যু নিবন্ধন নং <span class="text-danger">*</span></label>
+                    <label for="death_reg_no">আয়ের উৎস <span class="text-danger">*</span></label>
                     <input type="text" class="form-control @error('death_reg_no') is-invalid @enderror" id="death_reg_no" name="death_reg_no"
                            value="{{ old('death_reg_no', $applicant['death_reg_no'] ?? '') }}" placeholder="মৃত্যু নিবন্ধন নং" required>
                     @error('death_reg_no') <span class="invalid-feedback">{{ $message }}</span> @enderror
