e1f25dcd84194d7930e55d95cd05e583e2774f4d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 00:15:27 +0600

    Commit Tue 11/04/2025  0:15:27.30 1322

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
                 <span class="float-right text-muted text-sm">3 mins</span>
               </a>
               <div class="dropdown-divider"></div>
-              <a href="#" class="dropdown-item">
+              {{-- <a href="#" class="dropdown-item">
                 <i class="fas fa-users mr-2"></i> 0 friend requests
                 <span class="float-right text-muted text-sm">12 hours</span>
               </a>
@@ -83,7 +83,7 @@
               <a href="#" class="dropdown-item">
                 <i class="fas fa-file mr-2"></i> 0 new reports
                 <span class="float-right text-muted text-sm">2 days</span>
-              </a>
+              </a> --}}
               <div class="dropdown-divider"></div>
               <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
             </div>
