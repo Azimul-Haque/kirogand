f5e9a36f9f763a1ef9b6c30044cfbedb217bf75b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 02:55:37 +0600

    Commit Tue 11/04/2025  2:55:37.25 27804

--- resources/views/dashboard/certificates/forms/new-voter.blade.php
+++ resources/views/dashboard/certificates/forms/new-voter.blade.php
@@ -55,9 +55,9 @@
                     @error('name') <span class="invalid-feedback">{{ $message }}</span> @enderror
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="death_date">মৃত্যুর তারিখ <span class="text-danger">*</span></label>
+                    <label for="death_date">জন্মতারিখ <span class="text-danger">*</span></label>
                     <input type="date" class="form-control @error('death_date') is-invalid @enderror" id="death_date" name="death_date"
-                           value="{{ old('death_date', $applicant['death_date'] ?? '') }}" max="{{ date('Y-m-d') }}" placeholder="মৃত্যুর তারিখ" data-date="" data-date-format="DD MMMM YYYY" required>
+                           value="{{ old('death_date', $applicant['death_date'] ?? '') }}" max="{{ date('Y-m-d') }}" placeholder="জন্মতারিখ" data-date="" data-date-format="DD MMMM YYYY" required>
                     @error('death_date') <span class="invalid-feedback">{{ $message }}</span> @enderror
                     {{-- <script type="text/javascript" src="{{ asset('js/jquery-for-dp.min.js') }}"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js"></script>
