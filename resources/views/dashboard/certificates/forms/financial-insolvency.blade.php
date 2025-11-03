43cf1d7923585278f747ad77bbb91fe3801d2d05
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 03:51:39 +0600

    Commit Tue 11/04/2025  3:51:39.29 29254

--- resources/views/dashboard/certificates/forms/financial-insolvency.blade.php
+++ resources/views/dashboard/certificates/forms/financial-insolvency.blade.php
@@ -101,9 +101,8 @@
                 <!-- সমস্যার বিবরণ -->
                 <div class="form-group col-md-4">
                     <label for="mother">মাতার নাম <span class="text-danger">*</span></label>
-                    <textarea type="text" class="form-control @error('mother') is-invalid @enderror" id="mother" name="mother"
-                           value="{{ old('mother', $applicant['mother'] ?? '') }}" placeholder="মাতার নাম" required>
-                           </textarea>
+                    <textarea class="form-control @error('mother') is-invalid @enderror" id="mother" name="mother"
+                           value="{{ old('mother', $applicant['mother'] ?? '') }}" placeholder="মাতার নাম" required></textarea>
                     @error('mother') <span class="invalid-feedback">{{ $message }}</span> @enderror
                 </div>
             </div>
