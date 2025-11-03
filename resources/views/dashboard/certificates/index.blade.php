410b7569bae38297da6ab9aeca1df3af7def241f
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 02:49:48 +0600

    Commit Tue 11/04/2025  2:49:48.23 5747

--- resources/views/dashboard/certificates/index.blade.php
+++ resources/views/dashboard/certificates/index.blade.php
@@ -185,7 +185,7 @@
         </div>
 
         <div class="col-lg-2 col-md-4 col-sm-6 col-6 service-box-container" data-service-id="5">
-            <a href="{{ route('dashboard.certificates.create', 'monthly-income') }}" class="text-decoration-none text-dark d-block">
+            <a href="{{ route('dashboard.certificates.create', 'yearly-income') }}" class="text-decoration-none text-dark d-block">
                 <div class="service-box">
                     <div class="icon-circle" style="background-color: #ff9800;"><i class="fas fa-money-bill-alt"></i></div> 
                     <h3 class="h5 font-weight-bold" style="color: var(--darker-color);">বাৎসরিক আয়ের প্রত্যয়ন</h3>
