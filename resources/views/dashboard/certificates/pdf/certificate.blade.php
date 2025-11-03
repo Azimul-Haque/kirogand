fc0a72b88e0b2febbd76f63db90c18897b98cd54
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 01:06:40 +0600

    Commit Tue 11/04/2025  1:06:40.31 2274

--- resources/views/dashboard/certificates/pdf/certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/certificate.blade.php
@@ -175,7 +175,7 @@
             </p>
         @elseif($certificate->certificate_type == 'death-certificate')
             <p class="info-paragraph" style="margin-top: 30px;">
-                এতদ্বারা প্রত্যয়ন করা যাচ্ছে যে, {{ $applicant['name'] ?? '--' }} ({{ $applicant['id_type'] ?? '--' }}: {{ $applicant['id_value'] ?? '--' }}), পিতা: {{ $applicant['father'] ?? '--' }}, মাতা: {{ $applicant['mother'] ?? '--' }}, গ্রাম: {{ $applicant['village'] ?? '--' }}, ডাকঘর: {{ $applicant['post_office'] ?? '--' }}, উপজেলা: {{ $union_info['upazila'] ?? '--' }}, জেলা: {{ $union_info['district'] ?? '--' }} অত্র {{ $applicant['union'] ?? '--' }}-এর {{ $applicant['ward'] ?? '--' }} ওয়ার্ডের স্থায়ী বাসিন্দা ছিলেন। উল্লেখ্য যে, তিনি গত {{ $union_info['death_date'] ?? '--' }} তারিখে 
+                এতদ্বারা প্রত্যয়ন করা যাচ্ছে যে, {{ $applicant['name'] ?? '--' }} ({{ $applicant['id_type'] ?? '--' }}: {{ $applicant['id_value'] ?? '--' }}), পিতা: {{ $applicant['father'] ?? '--' }}, মাতা: {{ $applicant['mother'] ?? '--' }}, গ্রাম: {{ $applicant['village'] ?? '--' }}, ডাকঘর: {{ $applicant['post_office'] ?? '--' }}, উপজেলা: {{ $union_info['upazila'] ?? '--' }}, জেলা: {{ $union_info['district'] ?? '--' }} অত্র {{ $applicant['union'] ?? '--' }}-এর {{ $applicant['ward'] ?? '--' }} ওয়ার্ডের স্থায়ী বাসিন্দা ছিলেন। উল্লেখ্য যে, তিনি গত {{ $union_info['death_date'] ?? '--' }} খ্রি: তারিখে মৃত্যুবরণ করেন। 
 
 
                  সে বর্তমানে অবিবাহিত। এটি আমার জানামতে সত্য।
