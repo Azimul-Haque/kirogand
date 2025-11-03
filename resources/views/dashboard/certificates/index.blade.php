5ca9f02aa79546493b2a40df78cc04b405bb56a0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 00:25:38 +0600

    Commit Tue 11/04/2025  0:25:38.28 14229

--- resources/views/dashboard/certificates/index.blade.php
+++ resources/views/dashboard/certificates/index.blade.php
@@ -157,6 +157,15 @@
             </a>
         </div>
 
+        <div class="col-lg-2 col-md-4 col-sm-6 col-6 service-box-container" data-service-id="5">
+            <a href="{{ route('dashboard.certificates.create', 'unmarried-certificate') }}" class="text-decoration-none text-dark d-block">
+                <div class="service-box">
+                    <div class="icon-circle" style="background-color: var(--primary-color);"><i class="fas fa-address-book"></i></div> 
+                    <h3 class="h5 font-weight-bold" style="color: var(--darker-color);">প্রত্যয়নপত্র</h3>
+                </div>
+            </a>
+        </div>
+
 
 
 
