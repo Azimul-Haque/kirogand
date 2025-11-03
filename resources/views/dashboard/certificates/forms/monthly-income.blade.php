d15745753372f2526c8c471275f4f08f004201b0
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 02:05:52 +0600

    Commit Tue 11/04/2025  2:05:52.25 23887

--- resources/views/dashboard/certificates/forms/monthly-income.blade.php
+++ resources/views/dashboard/certificates/forms/monthly-income.blade.php
@@ -48,10 +48,10 @@
                     <label for="income_source">উপার্জনকারী <span class="text-danger">*</span></label>
                     <select class="form-control @error('income_source') is-invalid @enderror" id="income_source" name="income_source" required>
                         <option value="" selected disabled>আয়ের ব্যক্তি নির্বাচন করুন</option>
-                        <option value="own" {{ old('income_source', $applicant['income_source']) == 'own' ? 'selected' : '' }}>নিজে</option>
-                        <option value="father" {{ old('income_source', $applicant['income_source']) == 'father' ? 'selected' : '' }}>পিতা</option>
-                        <option value="mother" {{ old('income_source', $applicant['income_source']) == 'mother' ? 'selected' : '' }}>মাতা</option>
-                        <option value="other" {{ old('income_source', $applicant['income_source']) == 'mother' ? 'selected' : '' }}>অভিভাবক</option>
+                        <option value="own" {{ old('income_source', $applicant['income_source'] ?? '') == 'own' ? 'selected' : '' }}>নিজে</option>
+                        <option value="father" {{ old('income_source', $applicant['income_source'] ?? '') == 'father' ? 'selected' : '' }}>পিতা</option>
+                        <option value="mother" {{ old('income_source', $applicant['income_source'] ?? '') == 'mother' ? 'selected' : '' }}>মাতা</option>
+                        <option value="other" {{ old('income_source', $applicant['income_source'] ?? '') == 'mother' ? 'selected' : '' }}>অভিভাবক</option>
                     </select>
                     @error('income_source')<span class="invalid-feedback">{{ $message }}</span>@enderror
                 </div>
