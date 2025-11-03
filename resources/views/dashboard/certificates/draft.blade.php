96198db13a7bfecd42f05ace730097bbd497ebad
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 03:47:33 +0600

    Commit Tue 11/04/2025  3:47:33.29 25043

--- resources/views/dashboard/certificates/draft.blade.php
+++ resources/views/dashboard/certificates/draft.blade.php
@@ -47,6 +47,8 @@
             @include('dashboard.certificates.draft.yearly-income')
           @elseif($certificate->certificate_type == 'new-voter')
             @include('dashboard.certificates.draft.new-voter')
+          @elseif($certificate->certificate_type == 'financial-insolvency')
+            @include('dashboard.certificates.draft.financial-insolvency')
           @endif
         </div>
       </div>
