e37a31d0577e0ecd641f7ef3a065a49b454724d2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 01:31:47 +0600

    Commit Tue 11/04/2025  1:31:47.27 3700

--- resources/views/dashboard/certificates/index.blade.php
+++ resources/views/dashboard/certificates/index.blade.php
@@ -161,7 +161,7 @@
             <a href="{{ route('dashboard.certificates.create', 'death-certificate') }}" class="text-decoration-none text-dark d-block">
                 <div class="service-box">
                     <div class="icon-circle" style="background-color: #dc3545;"><i class="fas fa-user-times"></i></div> 
-                    <h3 class="h5 font-weight-bold" style="color: var(--darker-color);">ভোটার এলাকা স্থানান্তর প্রত্যয়ন</h3>
+                    <h3 class="h5 font-weight-bold" style="color: var(--darker-color);">ভোটার এলাকা স্থানান্তর</h3>
                 </div>
             </a>
         </div>
