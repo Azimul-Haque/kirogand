a58225d97d3718bd5d1d9c22b4cf864e229cb351
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 02:25:00 +0600

    Commit Tue 11/04/2025  2:25:00.26 23615

--- resources/views/dashboard/certificates/pdf/certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/certificate.blade.php
@@ -198,7 +198,7 @@
             <p class="info-paragraph" style="margin-top: 30px;">
                 এতদ্বারা প্রত্যয়ন করা যাচ্ছে যে, {{ $applicant['name'] ?? '--' }} ({{ $applicant['id_type'] ?? '--' }}: {{ $applicant['id_value'] ?? '--' }}), পিতা: {{ $applicant['father'] ?? '--' }}, মাতা: {{ $applicant['mother'] ?? '--' }}, গ্রাম: {{ $applicant['village'] ?? '--' }}, ওয়ার্ড: {{ $applicant['ward'] ?? '--' }}, ডাকঘর: {{ $applicant['post_office'] ?? '--' }}, উপজেলা: {{ $union_info['upazila'] ?? '--' }}, জেলা: {{ $union_info['district'] ?? '--' }}-কে আমি ব্যক্তিগতভাবে চিনি ও জানি। তিনি অত্র {{ $applicant['union'] ?? '--' }}-এর স্থায়ী বাসিন্দা ও জন্মগতভাবে বাংলাদেশের নাগরিক।
                 @if(($applicant['earner'] ?? '--') == 'own')
-                    উল্লেখ্য যে, তার পেশা: {{ $applicant['profession'] }}। আমার জানামতে, মাসিক গড় আয় {{ $applicant['income'] }}।
+                    উল্লেখ্য যে, তার পেশা: {{ $applicant['profession'] }}। আমার জানামতে, মাসিক গড় আয় {{ $applicant['income'] }}। এটি আমার জানামতে সত্য।
                 @else
 
                 @endif
