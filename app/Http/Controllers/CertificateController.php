4d79b90d30aa7305b2090d01d312b16e1fae115c
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-04 02:57:52 +0600

    Commit Tue 11/04/2025  2:57:52.25 23762

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -402,7 +402,7 @@ class CertificateController extends Controller
             ]);
         } elseif($certificate->certificate_type == 'new-voter') {
             $applicantData = $request->only([
-                'name', 'earner', 'profession', 'income', 'father', 'mother', 'id_type', 'id_value', 'mobile',
+                'name', 'dob', 'father', 'mother', 'id_type', 'id_value', 'mobile',
                 'village', 'ward', 'post_office', 'union'
             ]);
         } else {
