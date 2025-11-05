0043b7e23c7db99be70a3cab7cc3ebc1bc81067b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-06 03:03:29 +0600

    Commit Thu 11/06/2025  3:03:29.27 8569

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -65,7 +65,7 @@
 
     @font-face {
         font-family: 'Tiro Bangla'; 
-        font-weight: 400;
+        font-weight: 500;
         font-style: normal;
         /*src: url('https://dnagorik.com/fonts/kalpurush-webfont.woff2') format('woff2'),
              url('https://dnagorik.com/fonts/kalpurush-webfont.woff') format('woff'); */
