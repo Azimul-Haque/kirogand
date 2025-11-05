51f0e550e4ec2e86fd03b441afa04a6af23374b7
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-06 02:20:52 +0600

    Commit Thu 11/06/2025  2:20:52.23 20154

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -277,9 +277,9 @@
                       <li><hr class="dropdown-divider"></li>
                       <li><a class="dropdown-item text-primary fw-bold" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fas fa-sign-out me-2"></i> লগআউট</a></li>
                     @else
-                      {{-- <li><a class="dropdown-item text-primary fw-bold" href="/citizen-login"><i class="fas fa-user me-2"></i> নাগরিক লগইন</a></li>
+                      {{-- <li><a class="dropdown-item text-info fw-bold" href="/citizen-login"><i class="fas fa-user me-2"></i> নাগরিক লগইন</a></li>
                       <li><hr class="dropdown-divider"></li> --}}
-                      <li><a class="dropdown-item text-info fw-bold" href="{{ route('office.login') }}"><i class="fas fa-user-tie me-2"></i> প্রশাসনিক একাউন্ট</a></li>
+                      <li><a class="dropdown-item text-primary fw-bold" href="{{ route('office.login') }}"><i class="fas fa-user-tie me-2"></i> প্রশাসনিক একাউন্ট</a></li>
                     @endif
                   </ul>
                   <form id="logout-form" action="/logout" method="POST" style="display: none;">
