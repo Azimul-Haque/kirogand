4e6b74cdc7cdb146b2d7d2e833d1e23c68a1e785
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-06 02:36:48 +0600

    Commit Thu 11/06/2025  2:36:48.23 13968

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -70,7 +70,7 @@
 
     body {
         /* Google Fonts থেকে পাওয়া ফন্ট-ফ্যামিলি ব্যবহার করুন, সাথে একটি ফলব্যাক দিন */
-        font-family: 'kalpurushs', sans-serif;
+        font-family: 'kalpurushs', Arial, sans-serif;
         line-height: 1.6;
         background-color: var(--white-bg);
         color: var(--darker-color);
