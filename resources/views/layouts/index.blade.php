818ec03cfbb668a5a96b6279a65dd50c1631d80d
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-06 02:58:34 +0600

    Commit Thu 11/06/2025  2:58:34.25 16161

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -44,7 +44,7 @@
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Tiro+Bangla:ital@0;1&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Bengali:wght@100..900&display=swap" rel="stylesheet">
 
 
   <!-- Load Bootstrap 5 CSS -->
