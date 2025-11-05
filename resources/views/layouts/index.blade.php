f743b3c02c4df730d55c0ff94c5f098cb3fbd8d5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-06 02:35:07 +0600

    Commit Thu 11/06/2025  2:35:07.31 22646

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -64,7 +64,7 @@
         font-weight: 400;
         font-style: normal;
         src: url({{ asset('fonts/') }}) format('woff2'),
-             url({{ asset('fonts/') }}) format('woff');
+             url('{{ asset('fonts/') }}') format('woff');
         font-display: swap;
     }
 
