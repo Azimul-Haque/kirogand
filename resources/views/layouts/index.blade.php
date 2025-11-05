c466e49f94d29fcb70a8f0ca80b2354ebaaf58dc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-06 02:43:34 +0600

    Commit Thu 11/06/2025  2:43:34.25 22861

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -62,7 +62,7 @@
     
 
     body {
-        font-family: 'kalpurush', Arial, sans-serif;
+        font-family: 'kalpurushss', Arial, sans-serif;
         line-height: 1.6;
         background-color: var(--white-bg);
         color: var(--darker-color);
