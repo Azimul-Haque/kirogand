9708f2bcca8bfb941b4950f72f435547665477ec
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-07 02:12:10 +0600

    Commit Fri 11/07/2025  2:12:10.22 2764

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -544,7 +544,7 @@ class IndexController extends Controller
         $image = imagecreatetruecolor($width, $height);
 
         // Define colors
-        $white = imagecolorallocate($image, 227, 242, 253);
+        $white = imagecolorallocate($image, 200, 242, 253);
         $black = imagecolorallocate($image, 0, 0, 0);
 
         // Fill the background with white
