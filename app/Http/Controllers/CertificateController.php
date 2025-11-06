6266d08f2df98715f4676e4b1e5cff8b05524333
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-07 02:34:19 +0600

    Commit Fri 11/07/2025  2:34:19.23 28453

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -69,7 +69,8 @@ class CertificateController extends Controller
                 return redirect()->route('index.index');
             }
             if(isPackageExpired(Auth::user()->localOffice->package_expiry_date)) {
-                
+                Session::flash('success', 'আপনার নিবন্ধন সফল হয়েছে। অনুমোদনের জন্য অপেক্ষা করুন। আপনার সাথে যোগাযোগ করা হবে। অথবা এই নম্বরে যোগাযোগ করুন: 01xxxxxxxxx');
+                return redirect()->route('index.index');
             }
         }
 
