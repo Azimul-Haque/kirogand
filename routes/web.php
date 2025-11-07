31b4d5e6d7b87cccdd6687fce849bf5adf81ccfb
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-07 23:54:07 +0600

    Commit Fri 11/07/2025 23:54:07.30 21117

--- routes/web.php
+++ routes/web.php
@@ -111,7 +111,7 @@ Route::get('/dashboard/users/{id}/activate', 'DashboardController@activateUser')
 
 // office dashboard users
 Route::get('/dashboard/localoffice/users', 'DashboardController@getLocalOfficeUsers')->name('dashboard.localoffice.users');
-Route::get('/dashboard/localoffice/users/{id}/certificates', 'DashboardController@getLocalOfficeUsers')->name('dashboard.localoffice.users.certificates');
+Route::get('/dashboard/localoffice/users/{id}/certificates', 'DashboardController@getLocalOfficeUsersCerts')->name('dashboard.localoffice.users.certificates');
 0
 Route::get('/dashboard/local-offices', 'DashboardController@getLocalOffices')->name('dashboard.local-offices');
 Route::get('/dashboard/local-offices/{search}', 'DashboardController@getLocalOfficesSearch')->name('dashboard.local-offices.search');
