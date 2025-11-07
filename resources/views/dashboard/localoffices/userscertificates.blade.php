98b4e3b3970130fbdcc9cbae859b12786db3bbea
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-08 00:27:03 +0600

    Commit Sat 11/08/2025  0:27:03.26 31813

--- resources/views/dashboard/localoffices/userscertificates.blade.php
+++ resources/views/dashboard/localoffices/userscertificates.blade.php
@@ -128,7 +128,7 @@
                                         <td>
                                             @if ($certificate->status == 1)
                                                 <span class="badge badge-success">অনুমোদিত</span><br/>
-                                                <small>ইস্যু: {{ $certificate->issued_at ? \Carbon\Carbon::parse($certificate->issued_at)->format('d-m-Y') : 'N/A' }}</small>
+                                                <small>ইস্যু: {{ $certificate->issued_at ? bangla(\Carbon\Carbon::parse($certificate->issued_at)->format('d F Y')) : 'N/A' }}</small>
                                             @elseif ($certificate->status == 0)
                                                 <span class="badge badge-warning">ড্রাফট / অপেক্ষমাণ</span>
                                             @else
