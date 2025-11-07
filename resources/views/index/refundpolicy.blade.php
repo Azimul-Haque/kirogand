19187927aca744d033d55228a8758305ff214662
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-07 14:01:14 +0600

    Commit Fri 11/07/2025 14:01:14.24 24908

--- resources/views/index/refundpolicy.blade.php
+++ resources/views/index/refundpolicy.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('content')
-<section style="padding-top: 150px; padding-bottom: 50px; background-color: var(--light-3);">
+<section class="service-section section-gap">
     <div class="container">
 	    <div class="row">
 	      <div class="col-md-12 col-12">
