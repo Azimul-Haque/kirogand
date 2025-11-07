a62267fbea72ae3d00e02032da4cddcd2b36e115
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-07 23:58:17 +0600

    Commit Fri 11/07/2025 23:58:17.25 7572

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -733,6 +733,22 @@ class DashboardController extends Controller
         $users = User::where('local_office_id', Auth::user()->local_office_id)
                      ->where('role', 'user')
                      ->paginate(15);
+        $userscount = User::where('name', 'LIKE', "%$search%")
+                          ->orWhere('email', 'LIKE', "%$search%")
+                          ->orWhere('mobile', 'LIKE', "%$search%")
+                          ->orWhere('nid', 'LIKE', "%$search%")
+                          ->orWhere('onesignal_id', 'LIKE', "%$search%")
+                          ->orderBy('id', 'desc')
+                          ->count();
+        $divisions = Division::all();
+        $localoffices = LocalOffice::all();
+        $users = User::where('name', 'LIKE', "%$search%")
+                     ->orWhere('email', 'LIKE', "%$search%")
+                     ->orWhere('mobile', 'LIKE', "%$search%")
+                     ->orWhere('nid', 'LIKE', "%$search%")
+                     ->orWhere('onesignal_id', 'LIKE', "%$search%")
+                     ->orderBy('id', 'desc')
+                     ->paginate(10);
                      
         
         return view('dashboard.localoffices.users')
