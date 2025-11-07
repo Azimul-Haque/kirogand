e522ba6106b98d60745078b0360364f0d2a0d644
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-07 23:43:08 +0600

    Commit Fri 11/07/2025 23:43:08.25 12539

--- resources/views/dashboard/localoffices/users.blade.php
+++ resources/views/dashboard/localoffices/users.blade.php
@@ -58,7 +58,7 @@
                 		<td>
                 			<a href="{{ route('dashboard.users.single', $user->id) }}" data-toggle="tooltip" data-original-title="টেস্ট">{{ $user->name }}</a><span class="badge @if($user->role == 'admin') bg-success @elseif($user->role == 'manager') bg-warning @else bg-info @endif">{{ checkrole($user->role) }}</span>
                 			<br/>
-                			<small class="text-black-50">{{ $user->mobile }}</small> 
+                			<small class="text-black-50">মোবাইল: {{ $user->mobile }}</small> 
                 			<br/>
                       <small><span>যোগদান: {{ date('d F, Y h:i A', strtotime($user->created_at)) }}</span></small><br/>
                       {{-- <small><span>প্যাকেজ: <b>{{ $user->localOffice != null ? $user->localOffice->package_expiry_date != null ? date('d F, Y', strtotime($user->localOffice->package_expiry_date)) : 'N/A' : 'N/A' }}</b></span></small> --}}
