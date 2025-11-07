8752085eb07f1bc06d27719d21dfae129c84e632
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-08 02:30:59 +0600

    Commit Sat 11/08/2025  2:30:59.30 16597

--- resources/views/dashboard/certificates/pdf/certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/certificate.blade.php
@@ -137,7 +137,7 @@
                                         <thead>
                                             <tr>
                                                 <th style="width: 8%;">ক্রমিক নং</th>
-                                                <th style="width: 25%;">নাম</th>
+                                                <th style="width: 30%;">নাম</th>
                                                 <th style="width: 15%;">সম্পর্ক</th>
                                                 <th style="width: 25%;">এনআইডি/জন্ম সনদ</th>
                                                 <th style="width: 15%;">জন্ম তারিখ</th>
