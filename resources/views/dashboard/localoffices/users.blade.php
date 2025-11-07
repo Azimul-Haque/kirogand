8305a03213400eede8dd111acef58638a27a9836
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-07 23:51:27 +0600

    Commit Fri 11/07/2025 23:51:27.28 12738

--- resources/views/dashboard/localoffices/users.blade.php
+++ resources/views/dashboard/localoffices/users.blade.php
@@ -60,7 +60,7 @@
                     <td>
                       {{ $user->localOffice != null ? $user->localOffice->name_bn : '' }}
                     </td>
-                    <td>{{ $user->certificates->count() }}</td>
+                    <td>{{ bangla($user->certificates->count()) }}</td>
                         {{-- Delete User Modal Code --}}
                         {{-- Delete User Modal Code --}}
                         <!-- Modal -->
