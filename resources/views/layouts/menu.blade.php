d1a734ff7b773a726f121fb6e0300c4e1db3de28
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-07 22:35:36 +0600

    Commit Fri 11/07/2025 22:35:36.27 30490

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -55,7 +55,7 @@
 <li class="nav-item">
     <a href="{{ route('dashboard.certificates.list') }}" class="nav-link {{ Request::is('dashboard/certificates-list') ? 'active' : '' }} {{ Request::is('dashboard/certificates-list/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-users"></i>
-        <p>সনদ তালিকা</p>
+        <p>নিবন্ধনকৃত ব্যবহারকারীগণ</p>
     </a>
 </li>
 @endif
