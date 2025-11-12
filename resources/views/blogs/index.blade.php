ca278112e2a50609efab13add1b0a5296af7f60b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 03:26:15 +0600

    Commit Thu 11/13/2025  3:26:15.22 15147

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -106,7 +106,7 @@
         </div>
 
     <!-- Paginating Links -->
-    <div class="d-flex justify-content-center mt-5">
+    <div class="d-flex justify-content-center ">
         {{ $blogs->links() }}
     </div>
 
