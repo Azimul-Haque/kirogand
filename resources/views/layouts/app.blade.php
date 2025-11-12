6019690a080036b3ea95594b9da01ae2bc645711
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 00:15:35 +0600

    Commit Thu 11/13/2025  0:15:35.26 8013

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
         }
 
         body {
-            font-family: 'kalpurush', Arial, sans-serif;
+            font-family: 'Noto Sans Bengali', Arial, sans-serif;
         }
     </style>
     @yield('third_party_stylesheets')
