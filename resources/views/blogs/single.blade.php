9a4c1d98f26a8add9eb58c1cb13496ed2d470c2e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 04:40:42 +0600

    Commit Thu 11/13/2025  4:40:42.30 31504

--- resources/views/blogs/single.blade.php
+++ resources/views/blogs/single.blade.php
@@ -207,9 +207,9 @@
 
     </div>
     </div>
-    <div class="modal-footer">
-    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-    </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
     </div>
     </div>
     </div>
