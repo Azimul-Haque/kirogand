ecd0c1e3e545e87fb682c58a315e727e8d661801
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 03:13:43 +0600

    Commit Thu 11/13/2025  3:13:43.26 29840

--- resources/views/dashboard/videos/index.blade.php
+++ resources/views/dashboard/videos/index.blade.php
@@ -205,6 +205,25 @@
                 </div>
             </div>
         </div>
+        
+        <div class="col-md-6 col-lg-4 mb-5">
+            <div class="card h-100 video-card shadow-lg">
+                <div class="thumbnail-container" data-toggle="modal" data-target="#videoModal" data-youtube-id="KIsT2ZNK8JQ">
+                    <img src="https://img.youtube.com/vi/KIsT2ZNK8JQ/hqdefault.jpg" alt="নিবন্ধন পদ্ধতি থাম্বনেইল">
+                    <div class="play-overlay">
+                        <i class="fab fa-youtube"></i>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h5 class="video-tutorial-title">ডি-নাগরিক প্ল্যাটফর্মে নিবন্ধন পদ্ধতি</h5>
+                    <p class="card-text">ডি-নাগরিকে প্রশাসনিক কর্তৃপক্ষ হিসেবে দ্রুত এবং সফলভাবে নিবন্ধন করার জন্য প্রয়োজনীয় সব ধাপ এই ভিডিওতে দেখুন।</p>
+                    <button type="button" class="btn btn-watch btn-block mt-auto" 
+                            data-toggle="modal" data-target="#videoModal" data-youtube-id="KIsT2ZNK8JQ">
+                        <i class="fas fa-video mr-2"></i> ভিডিও দেখুন
+                    </button>
+                </div>
+            </div>
+        </div>
 
       </div>
 
