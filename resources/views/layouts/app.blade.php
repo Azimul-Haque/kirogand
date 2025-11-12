77bdf405b2c2cfc59baec4a9ed5be4fabeff2c72
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 00:11:18 +0600

    Commit Thu 11/13/2025  0:11:18.27 4007

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,8 @@
         font-family: 'kalpurush'; 
         font-weight: 400;
         font-style: normal;
-        /*src: url('https://dnagorik.com/fonts/kalpurush-webfont.woff2') format('woff2'),
-             url('https://dnagorik.com/fonts/kalpurush-webfont.woff') format('woff'); */
+        src: url('https://dnagorik.com/fonts/kalpurush-webfont.woff2') format('woff2'),
+             url('https://dnagorik.com/fonts/kalpurush-webfont.woff') format('woff');
         font-display: swap;
     }
 
