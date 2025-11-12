08ed7c74c6863a1475532f52e04733b0975123b2
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 04:12:54 +0600

    Commit Thu 11/13/2025  4:12:54.22 30043

--- resources/views/dashboard/blogs/index.blade.php
+++ resources/views/dashboard/blogs/index.blade.php
@@ -5,7 +5,7 @@
 <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/select2-bootstrap4.min.css') }}" rel="stylesheet" />
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="{{ asset('js/select2.full.min.js') }}"></script>
+{{-- <script src="{{ asset('js/select2.full.min.js') }}"></script> --}}
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
