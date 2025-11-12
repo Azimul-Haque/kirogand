7b9a983b36216f308c9fd959daaf156a5d979e23
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 00:22:29 +0600

    Commit Thu 11/13/2025  0:22:29.28 1978

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -35,7 +35,7 @@ use Illuminate\Support\Carbon;
 
     function limit_text($text, $limit) {
         // $pos=strpos($text, ' ', $limit);
-        // $text = substr($text,0,$pos ); 
+        // $text = substr($text,0,$pos); 
 
         if (str_word_count($text, 0) > $limit) {
             $words = str_word_count($text, 2);
@@ -49,22 +49,22 @@ use Illuminate\Support\Carbon;
         $en = array(1,2,3,4,5,6,7,8,9,0);
         $bn = array('১','২','৩','৪','৫','৬','৭','৮','৯','০');
         $str = str_replace($en, $bn, $str);
-        $en = array( 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' );
-        $en_short = array( 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' );
-        $bn = array( 'জানুয়ারি', 'ফেব্রুয়ারি', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'অগাস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর' );
-        $str = str_replace( $en, $bn, $str );
-        $str = str_replace( $en_short, $bn, $str );
+        $en = array( 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        $en_short = array( 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+        $bn = array( 'জানুয়ারি', 'ফেব্রুয়ারি', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'অগাস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর');
+        $str = str_replace( $en, $bn, $str);
+        $str = str_replace( $en_short, $bn, $str);
         $en = array('Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday');
         $en_short = array('Sat','Sun','Mon','Tue','Wed','Thu','Fri');
         $bn_short = array('শনি', 'রবি','সোম','মঙ্গল','বুধ','বৃহঃ','শুক্র');
         $bn = array('শনিবার','রবিবার','সোমবার','মঙ্গলবার','বুধবার','বৃহস্পতিবার','শুক্রবার');
-        $str = str_replace( $en, $bn, $str );
-        $str = str_replace( $en_short, $bn_short, $str );
-        $en = array( 'am', 'pm' );
-        $bn = array( 'পূর্বাহ্ন', 'অপরাহ্ন' );
-        $en = array( 'AM', 'PM' );
-        $bn = array( 'পূর্বাহ্ন', 'অপরাহ্ন' );
-        $str = str_replace( $en, $bn, $str );
+        $str = str_replace( $en, $bn, $str);
+        $str = str_replace( $en_short, $bn_short, $str);
+        $en = array( 'am', 'pm');
+        $bn = array( 'পূর্বাহ্ন', 'অপরাহ্ন');
+        $en = array( 'AM', 'PM');
+        $bn = array( 'পূর্বাহ্ন', 'অপরাহ্ন');
+        $str = str_replace( $en, $bn, $str);
         return $str;
   }
   
