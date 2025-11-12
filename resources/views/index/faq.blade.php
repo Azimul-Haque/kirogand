295bd36e2061fee8090218be8f3de487aec8403e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 03:42:06 +0600

    Commit Thu 11/13/2025  3:42:06.23 30113

--- resources/views/index/faq.blade.php
+++ resources/views/index/faq.blade.php
@@ -148,11 +148,11 @@
             <!-- Question 3: Importance of D-Nagorik -->
             <div class="accordion-item">
                 <h2 class="accordion-header" id="headingThree">
-                    <button class="accordion-button " type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                         <span class="me-2">৩.</span> ডি-নাগরিক কেন গুরুত্বপূর্ণ?
                     </button>
                 </h2>
-                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#faqAccordion">
+                <div id="collapseThree" class="accordion-collapse " aria-labelledby="headingThree" data-bs-parent="#faqAccordion">
                     <div class="accordion-body">
                         ডি-নাগরিক নাগরিক সেবা গ্রহণের একটি দ্রুত ও নির্ভরযোগ্য মাধ্যম। এটি সময়, খরচ এবং জটিলতা কমিয়ে নাগরিক সেবাকে সহজ ও ডিজিটাল করেছে।
                     </div>
