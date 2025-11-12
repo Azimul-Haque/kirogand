23100eb7db010ba1f8245dfe57668c0636f89aa1
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-11-13 03:29:01 +0600

    Commit Thu 11/13/2025  3:29:01.22 1791

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('meta-data')
     <meta name="description" content="ডি-নাগরিক: অনলাইনে ডিজিটাল সনদপত্র, প্রত্যয়ন ও সরকারি সেবা নিতে এখনই যুক্ত হোন ডিজিটাল নাগরিকের সাথে—বাংলাদেশের সেরা সনদ ও প্রত্যয়ন সেবা প্ল্যাটফর্ম। Developed By A. H. M. Azimul Haque.">
-    <meta name="keywords" content="BCS, Bangladesh Civil Service, NSI, দুদক, বাংলাদেশ ব্যাংক, অন্যান্য ব্যাংক, প্রাথমিক শিক্ষক নিয়োগ, Primary Exam, Job Circular, Bank Job Circular, Bank Job Exam, BCS Circular, বিসিএস পরীক্ষা, বার কাউন্সিল পরীক্ষা, জুডিশিয়াল পরীক্ষা, Judicial Exam, bcs book list, bcs book suggestion, BCS Preparation Books, বিসিএস প্রিলিমিনারি বই তালিকা, বিসিএস বই তালিকা, বিসিএস লিখিত বই তালিকা, bcs preliminary book list, bcs written book list, বিসিএস প্রিলিমিনারি পরীক্ষার সিলেবাস, বিসিএস পরীক্ষার সিলেবাস">
+    <meta name="keywords" content="ডি-নাগরিক, ডিজিটাল নাগরিক, সনদপত্র, প্রত্যয়ন, জন্ম নিবন্ধন, মৃত্যু নিবন্ধন, অনলাইন সেবা, স্থানীয় সরকার, ইউনিয়ন পরিষদ, পৌরসভা, ডিজিটাল সেবা, বাংলাদেশ">
     <!-- Structured data JSON-LD (optional but highly recommended) -->
     <script type="application/ld+json">
         {
