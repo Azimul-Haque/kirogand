4fdc4816221a6200bf2f92ff99bae0b180a35600
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-05 00:44:40 +0600

    Commit Fri 12/05/2025  0:44:40.26 29360

--- resources/views/dashboard/certificates/forms/heir-certificate.blade.php
+++ resources/views/dashboard/certificates/forms/heir-certificate.blade.php
@@ -125,9 +125,9 @@
             </div>
             <div class="row">
                 <div class="form-group col-md-6">
-                    <label for="father">পিতার নাম <span class="text-danger">*</span></label>
+                    <label for="father">পিতা/স্বামীর নাম <span class="text-danger">*</span></label>
                     <input type="text" class="form-control @error('father') is-invalid @enderror" id="father" name="father"
-                        value="{{ old('father', $applicant['father'] ?? '') }}" placeholder="পিতার নাম" required>
+                        value="{{ old('father', $applicant['father'] ?? '') }}" placeholder="পিতা/স্বামীর নাম" required>
                     @error('father') <span class="invalid-feedback">{{ $message }}</span> @enderror
                 </div>
                 <div class="form-group col-md-6">
