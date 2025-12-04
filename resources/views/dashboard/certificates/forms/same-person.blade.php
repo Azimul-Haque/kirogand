cf566e3adedd27567bde820304dfc01e9c5441fc
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-05 00:45:22 +0600

    Commit Fri 12/05/2025  0:45:22.27 21359

--- resources/views/dashboard/certificates/forms/same-person.blade.php
+++ resources/views/dashboard/certificates/forms/same-person.blade.php
@@ -90,11 +90,11 @@
             </div>
 
             <div class="row">
-                <!-- পিতার নাম -->
+                <!-- পিতা/স্বামীর নাম -->
                 <div class="form-group col-md-6">
-                    <label for="father">পিতার নাম <span class="text-danger">*</span></label>
+                    <label for="father">পিতা/স্বামীর নাম <span class="text-danger">*</span></label>
                     <input type="text" class="form-control @error('father') is-invalid @enderror" id="father" name="father"
-                           value="{{ old('father', $applicant['father'] ?? '') }}" placeholder="পিতার নাম" required>
+                           value="{{ old('father', $applicant['father'] ?? '') }}" placeholder="পিতা/স্বামীর নাম" required>
                     @error('father') <span class="invalid-feedback">{{ $message }}</span> @enderror
                 </div>
                 <!-- মাতার নাম -->
@@ -141,7 +141,7 @@
             {{-- <div class="mt-3 mb-3">
                 <label for="editor">সনদের ভাষা: (ব্লক করা অংশগুলো বাদে এডিট করুন)</label>
                 <div id="editor" contenteditable="true" name="main_content" spellcheck="false" required>
-                    <p>এতদ্বারা প্রত্যয়ন করা যাচ্ছে যে, <span class="protected-token" contenteditable="false">[নাম]</span>, পিতা: <span class="protected-token" contenteditable="false">[পিতার নাম]</span>, মাতা: <span class="protected-token" contenteditable="false">[মাতার নাম]</span>, গ্রাম: <span class="protected-token" contenteditable="false">[গ্রাম]</span>, ওয়ার্ড: <span class="protected-token" contenteditable="false">[ওয়ার্ড]</span>, ডাকঘর: <span class="protected-token" contenteditable="false">[ডাকঘর]</span>, উপজেলা: <span class="protected-token" contenteditable="false">[উপজেলা]</span>, জেলা: <span class="protected-token" contenteditable="false">[জেলা]</span>-কে আমি ব্যক্তিগতভাবে চিনি ও জানি। তিনি অত্র <span class="protected-token" contenteditable="false">[ইউনিয়ন]</span>-এর স্থায়ী বাসিন্দা ও জন্মগতভাবে বাংলাদেশের নাগরিক। আমার জানামতে, তিনি কোন প্রকার সমাজবিরোধী বা রাষ্ট্রবিদ্রোহমূলক কর্মকাণ্ডের সাথে জড়িত ছিলেন না বা নেই।
+                    <p>এতদ্বারা প্রত্যয়ন করা যাচ্ছে যে, <span class="protected-token" contenteditable="false">[নাম]</span>, পিতা/স্বামী: <span class="protected-token" contenteditable="false">[পিতা/স্বামীর নাম]</span>, মাতা: <span class="protected-token" contenteditable="false">[মাতার নাম]</span>, গ্রাম: <span class="protected-token" contenteditable="false">[গ্রাম]</span>, ওয়ার্ড: <span class="protected-token" contenteditable="false">[ওয়ার্ড]</span>, ডাকঘর: <span class="protected-token" contenteditable="false">[ডাকঘর]</span>, উপজেলা: <span class="protected-token" contenteditable="false">[উপজেলা]</span>, জেলা: <span class="protected-token" contenteditable="false">[জেলা]</span>-কে আমি ব্যক্তিগতভাবে চিনি ও জানি। তিনি অত্র <span class="protected-token" contenteditable="false">[ইউনিয়ন]</span>-এর স্থায়ী বাসিন্দা ও জন্মগতভাবে বাংলাদেশের নাগরিক। আমার জানামতে, তিনি কোন প্রকার সমাজবিরোধী বা রাষ্ট্রবিদ্রোহমূলক কর্মকাণ্ডের সাথে জড়িত ছিলেন না বা নেই।
                     </p>
                     <p>
                         তার নৈতিক চরিত্র ভালো। আমি তার সর্বাঙ্গীন মঙ্গল কামনা করি।
