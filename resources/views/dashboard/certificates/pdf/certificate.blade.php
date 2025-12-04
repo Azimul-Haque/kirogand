cf946dfab3c7e540563a465f1769b1c7a058548b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-05 00:26:16 +0600

    Commit Fri 12/05/2025  0:26:16.25 9852

--- resources/views/dashboard/certificates/pdf/certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/certificate.blade.php
@@ -104,7 +104,7 @@
             </p>
 
             {{-- Beneficiary Table (Using Heirs Data) --}}
-            <div style="text-align: center; margin-bottom: 5px; font-weight: bold; font-size: 16px; margin-top: 3px; font-weight: bold;">
+            <div style="text-align: center; margin-bottom: 5px; font-weight: bold; font-size: 16px; margin-top: 0px; font-weight: bold;">
                 ওয়ারিশগণের নাম
             </div>
             <table class="beneficiary-table">
