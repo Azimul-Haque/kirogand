f91819bfb629b6256cb0fb0a1c97a42494aa58bd
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-06 16:02:31 +0600

    Commit Sat 12/06/2025 16:02:31.26 4738

--- resources/views/dashboard/certificates/pdf/certificate.blade.php
+++ resources/views/dashboard/certificates/pdf/certificate.blade.php
@@ -92,7 +92,7 @@
         {{-- Certificate Title --}}
         <table style="width: 100%; margin-top: 5px;">
             <tr>
-                <td width="30%">@if($certificate->memo) স্মারক নং - {{ $certificate->memo }} @else সনদ নং - {{ $certificate->unique_serial }} @endif</td>
+                <td width="30%"></td>
                 <td style="text-align: center;">@if($certificate->memo) স্মারক নং - {{ $certificate->memo }} @else সনদ নং - {{ $certificate->unique_serial }} @endif</td>
                 <td style="text-align: right; font-size: 14px;">তারিখ: {{ $certificate->issued_at != null ?  bangla(date('d-m-Y', strtotime($certificate->issued_at))) : bangla(date('d-m-Y')) }}</td>
             </tr>
