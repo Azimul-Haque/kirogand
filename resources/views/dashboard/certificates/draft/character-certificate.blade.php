43da829af5cf65c42f43b00bfe3b1e2ace926154
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-06 16:32:27 +0600

    Commit Sat 12/06/2025 16:32:27.28 8272

--- resources/views/dashboard/certificates/draft/character-certificate.blade.php
+++ resources/views/dashboard/certificates/draft/character-certificate.blade.php
@@ -84,7 +84,7 @@
                                         $imagePath = public_path('images/certificate-images/' . $image);
 
                                         // 2. Check if the file name is stored AND if the physical file exists.
-                                        $imageExists = $payload['image'] && File::exists($imagePath);
+                                        $imageExists = $image && File::exists($imagePath);
                                     @endphp
                                     @if ($imageExists)
                                         <img 
