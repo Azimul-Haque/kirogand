f9d7a0858f2b4ecdf4ebbb89bd34d96261c1d93e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-12-06 15:30:11 +0600

    Commit Sat 12/06/2025 15:30:11.26 22050

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -275,8 +275,8 @@ class CertificateController extends Controller
             $image      = $request->file('image');
             $filename   = strtolower($certificate_type) . '-image-' . time() . '.' . "png";
             $location   = public_path('images/certificate-images/' . $filename);
-            Image::make($image)->fit(300, 300)->save($location);
-            Image::make($image)->fit(450, 450)->opacity(15)->save($location_back);
+            Image::make($image)->fit(250, 200)->save($location);
+            // Image::make($image)->fit(450, 450)->opacity(15)->save($location_back);
             $localoffice->image = $filename;
         }
 
